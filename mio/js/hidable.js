4028b0e8e4637692d76ce0be3840cfb637e517ba
victor j. perez <vijapefi@gmail.com>
2018-10-03 14:42:40 -0400

    notHidable instead of primario class

--- mio/js/hidable.js
+++ mio/js/hidable.js
@@ -1,11 +1,11 @@
 jQuery(document).ready(
 	function(){
-		var $todosLosPrimario = jQuery('.primario');
+		var $todosLosNotHidable = jQuery('.notHidable');
 		var $todosLosHidable = jQuery('.hidable');
 		
 		$todosLosHidable.hide();
 
-		$todosLosPrimario.on('click', function(evento){
+		$todosLosNotHidable.on('click', function(evento){
 			var $toToggle = jQuery(evento.currentTarget).children('.hidable');
 			$toToggle.toggle()
 		});			
