11453cc50824179d480c877f70b706a5e6bdfabc
victor j. perez <vijapefi@gmail.com>
2018-10-06 10:05:21 -0400

    hidable.js now runs as part of getMainContent.js

--- mio/js/hidable.js
+++ mio/js/hidable.js
@@ -1,5 +1,5 @@
-jQuery(document).ready(
-	function(){
+//this code now runs after an ajaxComplete in the faq section of getMainContent.js
+jQuery(document).ready(function(){
 		var $todosLosNotHidable = jQuery('.notHidable');
 		var $todosLosHidable = jQuery('.hidable');
 		
@@ -7,8 +7,6 @@ jQuery(document).ready(
 
 		$todosLosNotHidable.on('click', function(evento){
 			var $toToggle = jQuery(evento.currentTarget).children('.hidable');
-			$toToggle.toggle()
+			$toToggle.toggle();
 		});			
-		
-	}
-);
+	});
