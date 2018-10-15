1fff0ac4ee73631c397951a96a98cc53194ba865
victor j. perez <vijapefi@gmail.com>
2018-10-15 12:11:27 -0400

    no json mimme type

--- mio/js/getMainContent.js
+++ mio/js/getMainContent.js
@@ -49,7 +49,7 @@ jQuery(document).ready(
 				var donde = jQuery.urlParam('donde');
 				jQuery.getJSON('uiTests/show15RandomOptionsTest.php', {que:que, donde:donde} )
 				.done(function(datos, estatusForDONE, xhrObjetoForDONE){
-					alert('datos: automatically parsed to object object by getJSON ' + datos + '\nxhrObjetoForDONE status ' + xhrObjetoForDONE.status + '\nxhrObjetoForDONE statustext ' + xhrObjetoForDONE.statusText + '\nestatusForDONE ' + estatusForDONE );
+					//alert('datos: automatically parsed to object object by getJSON ' + datos + '\nxhrObjetoForDONE status ' + xhrObjetoForDONE.status + '\nxhrObjetoForDONE statustext ' + xhrObjetoForDONE.statusText + '\nestatusForDONE ' + estatusForDONE );
 					var mainDeOpciones = '<div id="main" class="contenido margen"><div id="opcionesfotos" class="ver-borde">';
 					jQuery.each(datos, function(fotoSrc, id){
 						mainDeOpciones += '<a href="portada.html?look=profile&id=' + id + '"><img class="ancho-sensi-cell-1de2 ancho-sensi-ipad-1de4 ver-borde" src="';
@@ -161,11 +161,12 @@ jQuery(document).ready(
 			case 'micuenta': 
 				var user = jQuery.urlParam('user');
 				var pass = jQuery.urlParam('pass');
-				jQuery.post('uiTests/showCuentaToUser.php', {user:user, pass:pass}, "json" )
+				jQuery.post('uiTests/showCuentaToUser.php', {user:user, pass:pass} )
 				.done(function(datosJSONStr, estatusForDONE, xhrObjetoForDONE){
-					alert(datosJSONStr);
+					//alert(datosJSONStr);
 					datosJSObj = JSON.parse(datosJSONStr);  
-					alert('datos json parsed as a js obj ' + datosJSObj + '\ndatos.id ' + datosJSObj.id+ '\nxhrObjetoForDONE status ' + xhrObjetoForDONE.status + '\nxhrObjetoForDONE statustext ' + xhrObjetoForDONE.statusText + '\nestatusForDONE ' + estatusForDONE );
+					//datosJSObj = datosJSONStr;
+					//alert('datos json str parsed as a js obj ' + datosJSObj + '\ndatos.id ' + datosJSObj.id+ '\nxhrObjetoForDONE status ' + xhrObjetoForDONE.status + '\nxhrObjetoForDONE statustext ' + xhrObjetoForDONE.statusText + '\nestatusForDONE ' + estatusForDONE );
 				    if(datosJSObj.id == -1){ //datos recieves id=-1 when user cannot be logged.
 						jQuery(window.location).attr('href', window.location.pathname + '?look=login');
 				    }else{ // Otherwise user is logged - datos has all the user data to show in micuenta
