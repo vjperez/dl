1d7c22bff4066bbb7c9464cbc96126deb544b405
victor j. perez <vijapefi@gmail.com>
2018-10-30 20:43:15 -0400

    affected rows comment

--- mio/practice/sql/doQuery/queries.php
+++ mio/practice/sql/doQuery/queries.php
@@ -7,9 +7,9 @@ if($resource){
 	$ul = array();
 	$ul[0] = pg_num_rows($resource);       // the first  row in the $ul array, is just a number
 	$ul[1] = pg_affected_rows($resource);  // the second row in the $ul array, is just a number
-	$index = 2;						  //so must start counting from 2 here
+	$index = 2;						       //so must start counting from 2 here
 	while($fila = pg_fetch_row($resource)){
-		$ul[$index] = $fila;		  //other rows are arrays
+		$ul[$index] = $fila;		       //other rows are arrays
 		$index++;
 	}
 	echo json_encode($ul);
