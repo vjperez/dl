2f7dae9aa36ccebe88a611c298206ee8f31e876d
victor j. perez <vijapefi@gmail.com>
2018-11-23 06:13:35 -0400

    now on first and last login when login in and registering

--- mio/escritos/lee/loginQuery.php
+++ mio/escritos/lee/loginQuery.php
@@ -1,5 +1,13 @@
 <?php
-$query = "SELECT 
+$query = "UPDATE 
+		usuario
+SET last_log = NOW()::date 
+WHERE username = '$user'
+AND password = '$pass'
+
+ ;
+
+	SELECT 
 	usuario_id
 FROM usuario 
 WHERE username = '$user'
--- mio/escritos/lee/registraQueries.php
+++ mio/escritos/lee/registraQueries.php
@@ -6,8 +6,10 @@ WHERE username = '$usertb'";
 
 //double query
 $queryRegisterUserReturningId = "INSERT INTO 
-	usuario(username, password) 
-	VALUES('$usertb', '$pass01'); 
+	usuario(username, password, first_log, last_log) 
+	VALUES('$usertb', '$pass01', NOW()::date, NOW()::date)
+	
+	; 
 	
 	SELECT currval('usuario_usuario_id_seq')";
 ?>
