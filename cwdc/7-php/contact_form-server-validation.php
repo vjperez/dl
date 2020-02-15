25f5261e53371dda776c02f143dc1b3e94591df4
victor j. perez <vijapefi@gmail.com>
2020-02-15 09:24:21 -0400

    contact form validation

--- cwdc/7-php/contact_form-server-validation.php
+++ cwdc/7-php/contact_form-server-validation.php
@@ -1,8 +1,17 @@
 <?php
+$phpValidation = '';
 if($_POST){
-    echo "el post tiene esto:  " . print_r( $_POST, true ) . '<br><br><br>';
+    $nombre      = $_POST['nombrename'];
+    $telefono    = $_POST['telefononame'];
+    $informacion = $_POST['informacionname'];
+    $nombreregexp      = "/^[\s\w]*[a-zA-Z]+[\s\w]*$/";
+    $telefonoregexp    = "/^[\s]*[0-9]{3}-[0-9]{3}-[0-9]{4}[\s]*$/";
+    $informacionregexp = "/^[:.?=&-\[\]\(\)!@\s\w\/]*[a-zA-Z]+[:.?=&-\[\]\(\)!@\s\w\/]*$/";
+    if(preg_match($nombreregexp, $nombre))           $phpValidation = $phpValidation . '<div class="alert alert-success" role="alert"> nombre: ' . $nombre . '</div>';             else $phpValidation = $phpValidation . '<div class="alert alert-danger" role="alert"> invalid nombre </div>';
+    if(preg_match($telefonoregexp, $telefono))       $phpValidation = $phpValidation . '<div class="alert alert-success" role="alert"> telefono: ' . $telefono . '</div>';         else $phpValidation = $phpValidation . '<div class="alert alert-danger" role="alert"> invalid telefono </div>';
+    if(preg_match($informacionregexp, $informacion)) $phpValidation = $phpValidation . '<div class="alert alert-success" role="alert"> informacion: ' . $informacion . '</div>';   else $phpValidation = $phpValidation . '<div class="alert alert-danger" role="alert"> invalid informacion </div>';    
 }else{
-    echo "el post tiene NADA.<br><br><br>";
+   //   $phpValidation = $phpValidation . "el post tiene NADA.<br><br><br>";
 }
 ?>
 
@@ -19,7 +28,10 @@ if($_POST){
     <title>contact form</title>
   </head>
   <body>
-  <div class="container">  
+  <div class="container mt-3">
+      <?php echo $phpValidation; ?>
+  </div>
+  <div class="container mt-3">  
     <div class="card bg-light text-dark">
       <div class="card-header">  
           <h1>Contactanos:</h1>
@@ -29,7 +41,7 @@ if($_POST){
           <div class="form-group mt-5">
               <label for="nombre" class="font-weight-bold text-info">Tu nombre</label>
               <input type="text" class="form-control" id="nombre" name="nombrename" placeholder="tita"
-              pattern="^[\s\w]*[a-zA-Z]{3,5}[\s\w]*$" maxlength="64" required>
+              pattern="^[\s\w]*[a-zA-Z]+[\s\w]*$" maxlength="64" required>
               <small class="text-muted">Formato: Letras, numeros, y/o espacios.</small> 
           </div>
           <div class="form-group mt-5">
@@ -42,11 +54,11 @@ if($_POST){
           <div class="form-group mt-5">
               <label for="informacion" class="font-weight-bold text-info">Informacion</label>
               <textarea class="form-control" id="informacion" rows="3" name="informacionname" 
-              placeholder="Tu facebook, twitter, web page y/o instagram" maxlength="140" required
-              pattern="^[:\/.?=&!@\s\w]*[a-zA-Z]+[:\/.?=&!@\s\w]*$">
+              placeholder="Tu facebook, twitter, web page, instagram y/o tu email" maxlength="140" required
+              pattern="^[:.?=&-\[\]\(\)!@\s\w\/]*[a-zA-Z]+[:.?=&-\[\]\(\)!@\s\w\/]*$">
               </textarea>
               <!-- pattern no funciona en html5 pero jQuery lo lee ; (attr pattern)-->
-              <small class="text-muted">Formato: Letras, numeros, espacios, y/o   / : . _ ? = & ! @</small>
+              <small class="text-muted">Formato: Letras, numeros, espacios, y/o   : . ? = & - [] () ! @ /</small>
           </div>
 
           <button type="submit" class="btn btn-info mt-5">Submit</button>
