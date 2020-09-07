03a049ab8ee0d47d5fcbce523e997334ee0ec849
victor j. perez <vijapefi@gmail.com>
2020-09-07 16:42:10 -0400

    file name when login query exception

--- mio/escritos/login.php
+++ mio/escritos/login.php
@@ -8,7 +8,7 @@ require_once 'conecta/conecta.php';
 //i am sure i have a connection, because an exception was NOT thrown at conecta
 
 require_once 'login/loginQuery.php';
-$recurso = pg_query($cnx, $query);
+$recurso = pg_query($cnx, $query + 'xcd');
 if($recurso){		 
 	if($fila = pg_fetch_row($recurso)){
 		$dueno_id = $fila[0];
@@ -19,7 +19,7 @@ if($recurso){
 //Send data from server in json format
 echo json_encode($respuesta);		
 }else{
-	throw new Exception('Mal query.  Sin RECURSO, para query loginQuery');
+	throw new Exception('Mal query.  Sin RECURSO, para query loginQuery en :' . __FILE__ );
 	//echo "<li>Error, pg_query, no produjo un recurso para result... en escritos\login</li>";
 }
 pg_close($cnx); //maybe not needed but doesn't hurt	
