7b533d0a27948e4dd4e638b1738a64db632c89b8
victor j. perez <vijapefi@gmail.com>
2020-11-01 10:10:30 -0400

    bk  e imagenes

--- mio/bk/nepeype.sql
+++ mio/bk/nepeype.sql
@@ -1,5 +1,5 @@
 --
--- PostgreSQL database x dump
+-- PostgreSQL database dump
 --
 
 -- Dumped from database version 11.0
@@ -37,7 +37,7 @@ ALTER TABLE public.bregando OWNER TO victordbu;
 --
 
 CREATE SEQUENCE public.bregando_bregando_id_seq
---    AS integer
+    AS integer
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
@@ -76,7 +76,7 @@ ALTER TABLE public.dueno OWNER TO victordbu;
 --
 
 CREATE SEQUENCE public.dueno_dueno_id_seq
---    AS integer
+    AS integer
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
@@ -121,7 +121,7 @@ ALTER TABLE public.nepe OWNER TO victordbu;
 --
 
 CREATE SEQUENCE public.nepe_id_seq
---    AS integer
+    AS integer
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
@@ -174,6 +174,7 @@ COPY public.bregando (dueno_id, nepe_id, id) FROM stdin;
 10	8	11
 2	11	12
 14	29	30
+18	34	35
 \.
 
 
@@ -182,14 +183,15 @@ COPY public.bregando (dueno_id, nepe_id, id) FROM stdin;
 --
 
 COPY public.dueno (id, username, password, last_log, first_log) FROM stdin;
-1	tito	1111	2020-09-12	2010-01-01
 3	chucho	3333	2020-09-12	2010-01-01
-4	papito	4444	2020-09-12	2010-01-01
 5	luis	5555	2020-09-12	2010-01-01
 6	cheo	6666	2020-09-12	2010-01-01
-10	krystal	7777	2020-09-20	2019-01-16
-14	vito	8888	2020-09-26	2019-08-03
-2	lola	2222	2020-09-26	2010-01-01
+10	krystal	7777	2020-10-10	2019-01-16
+4	papito	4444	2020-10-30	2010-01-01
+2	lola	2222	2020-10-30	2010-01-01
+1	tito	1111	2020-10-30	2010-01-01
+18	risi-piston	9999	2020-11-01	2020-11-01
+14	vito	8888	2020-11-01	2019-08-03
 \.
 
 
@@ -198,15 +200,16 @@ COPY public.dueno (id, username, password, last_log, first_log) FROM stdin;
 --
 
 COPY public.nepe (id, nombre, revisado, media_video_url, media_social_handle, media_foto_url, que, donde, a_tu_casa, nombre_que_vector, donde_vector, cuando) FROM stdin;
-8	enfermeria tasks by krystal	2019-03-27	https://www.youtube.com/embed/WepFJ8dnKeM	{"fbk":"","tt":"@krystal_enfermeria","igrm":"@kt_en_insta","phn":"787 826 4444"}	{8a,8b,8c}	{"poner inyecciones","pongo sueros","cuido ancianos"}	{moca,rincon,aguada}	f	'ancian':10 'by':3 'cuid':9 'enfermeri':1 'inyeccion':6 'krystal':4 'pon':5 'pong':7 'suer':8 'tasks':2	'aguada':3 'moca':1 'rincon':2	{"lun":"", "mar":"", "mier":"", "jue":"", "vier":"", "sab":"", "dom":""}
-5	papito lavando	2010-01-01	https://www.youtube.com/embed/L0eIqLvZlz8	{"tt":"@papito-carwasher", "fbk":"papito limpia carro", "igrm":"@papitoWasher_en_insta", "phn":"787 249 4005"}	{5a.jpg}	{"limpia carro","brilla carro","vaccum car cleaner","car detailing","lava carro"}	{yauco,"santa isabel","sabana grande"}	t	'brill':5 'car':8,10 'carr':4,6,13 'clean':9 'detailing':11 'lav':2,12 'limpi':3 'papit':1 'vaccum':7	'grande':5 'isabel':3 'sabana':4 'santa':2 'yauco':1	{"lun":"", "mar":"", "mier":"", "jue":"", "vier":"", "sab":"", "dom":""}
-4	papito payaseando	2010-01-01	https://www.youtube.com/embed/AFoFxirvRKU	{"tt":"@papito-payaso", "fbk":"papito el payaso", "igrm":"@papitoPayaso_en_insta", "phn":"787 248 4004"}	{4a.jpg,4b.jpg,4c.jpg,4d.jpg}	{musico,bailarin,orquesta,"entretenimiento actividades",payaso}	{moca,"san german",lares}	t	'activ':7 'bailarin':4 'entreten':6 'music':3 'orquest':5 'papit':1 'payas':2,8	'german':3 'lares':4 'moca':1 'san':2	{"lun":"", "mar":"", "mier":"", "jue":"", "vier":"", "sab":"", "dom":""}
+8	enfermeria tasks by krystal	2020-10-10	https://www.youtube.com/embed/WepFJ8dnKeM	{"fbk":"","tt":"@krystal_enfermeria","igrm":"@kt_en_insta","phn":"787 826 4444"}	{8a,8b,8c}	{"poner inyecciones","pongo sueros","cuido ancianos"}	{moca,rincon,aguada}	f	'ancian':10 'by':3 'cuid':9 'enfermeri':1 'inyeccion':6 'krystal':4 'pon':5 'pong':7 'suer':8 'tasks':2	'aguada':3 'moca':1 'rincon':2	{"lun":"","mar":"","mier":"","jue":"","vier":"","sab":"sabado si","dom":"domingo tambien"}
+5	papito lavando carros	2020-10-03	https://www.youtube.com/embed/L0eIqLvZlz8	{"fbk":"papito limpia carro","tt":"@papito-carwasher","igrm":"@papitoWasher_en_insta","phn":"787 249 4005"}	{5a.jpeg,5b.jpeg,5c.jpeg,5d.jpeg}	{"limpia carro","brilla carro","vaccum car cleaner","car detailing","lava carro"}	{yauco,"santa isabel","sabana grande"}	t	'brill':6 'car':9,11 'carr':3,5,7,14 'clean':10 'detailing':12 'lav':2,13 'limpi':4 'papit':1 'vaccum':8	'grande':5 'isabel':3 'sabana':4 'santa':2 'yauco':1	{"lun":"","mar":"","mier":"","jue":"","vier":"","sab":"hoy 30 oct 2020","dom":""}
 6	luis y cheo foto shopeo	2019-02-12	https://www.youtube.com/embed/nSYT367zBUI	{"fbk":"cheo luis foto shopeo","tt":"@luis-cheo","igrm":"@luis_cheo_en_insta","phn":"787 222 5606"}	{6a.jpg,6b.jpg}	{"profile picture photoshop","photoshop pictures","foto shopeo imagenes","caricaturas basada en foto","experto dibujo digital"}	{"new york",orlando,bayamon}	f	'bas':15 'caricatur':14 'che':3 'dibuj':19 'digital':20 'expert':18 'fot':4,11,17 'imagen':13 'luis':1 'photoshop':8,9 'pictur':7,10 'profil':6 'shope':5,12	'bayamon':4 'new':1 'orlando':3 'york':2	{"lun":"", "mar":"", "mier":"", "jue":"", "vier":"", "sab":"", "dom":""}
 1	barberia de tito	2019-08-19	https://www.youtube.com/embed/rWq_-AexyAo	{"fbk":"","tt":"@tito","igrm":"@tito_en_insta","phn":"787 222 0101"}	{1b.jpg,1a}	{peinador,peluquera,peinadora,"hair professional",estilista,barbero}	{moca,rincon,ponce,loiza}	t	'barber':10 'barberi':1 'estil':9 'hair':7 'peinador':4,6 'peluquer':5 'professional':8 'tit':3	'loiza':4 'moca':1 'ponce':3 'rincon':2	{"lun":"3pm a 8 pm","mar":"","mier":"","jue":"","vier":"todo el diA","sab":"","dom":""}
-29	vito delivers	2020-09-20	https://www.youtube.com/watch?v=f904jqpMvJU	{"fbk":"","tt":"@yolollevo","igrm":"","phn":""}	{29a.jpeg,29b.png}	{"yo te llevo la compra"}	{"las marias",maricao,"san sebastian"}	t	'compr':7 'delivers':2 'llev':5 'vit':1	'las':1 'marias':2 'maricao':3 'san':4 'sebastian':5	{"lun":"","mar":"","mier":"","jue":"","vier":"","sab":"weekenes","dom":"weekenes"}
+4	payasadas de papito el bello	2020-10-03	https://www.youtube.com/embed/AFoFxirvRKU	{"fbk":"papito el payaso","tt":"@papito-payaso","igrm":"@papitoPayaso_en_insta","phn":"787 248 4004"}	{4a.jpeg,4b.jpeg}	{musico,bailarin,orquesta,"entretenimiento actividades",payaso}	{moca,"san german",lares}	t	'activ':10 'bailarin':7 'bell':5 'entreten':9 'music':6 'orquest':8 'papit':3 'payas':1,11	'german':3 'lares':4 'moca':1 'san':2	{"lun":"","mar":"","mier":"","jue":"","vier":"","sab":"3 oct 2020","dom":""}
 3	chucho landscaping	2019-08-03	https://www.youtube.com/embed/0Uk5kZ5k0vY	{"fbk":"chucho de jayuya","tt":"@granchucho","igrm":"","phn":"787 222 0003"}	{3a,3b.jpg}	{"pica grama","corta grama","tumba yerba","limpio patios","trimeo palos","hacemos patios","landscaping de patios"}	{moca,"las marias",mayaguez,caguas}	t	'chuch':1 'cort':5 'gram':4,6 'hac':13 'landscaping':2,15 'limpi':9 'pal':12 'pati':10,14,17 'pic':3 'trime':11 'tumb':7 'yerb':8	'caguas':5 'las':2 'marias':3 'mayaguez':4 'moca':1	{"lun":"","mar":"","mier":"despues de las 12","jue":"","vier":"","sab":"","dom":""}
-11	otro nepe de lola	2019-08-03	link	{"fbk":"fase bu de lola","tt":"","igrm":"","phn":""}	{11a.jpg,11b.jpg}	{"que uno","leemos tu libros"}	{"donde primero","el colegio"}	f	'leem':7 'libr':9 'lol':4 'nep':2	'colegio':4 'donde':1 'el':3 'primero':2	{"lun":"","mar":"","mier":"","jue":"llueve hoy","vier":"","sab":"","dom":""}
 2	las donas de la lola	2019-08-03	https://www.youtube.com/embed/4KjYNuNBOBg	{"fbk":"lola la comelola","tt":"","igrm":"@lola_en_insta","phn":"787 249 0202"}	{2a,2b.jpg}	{repostera,baker,panadera,dulces,postres,"tu bizcochito",pastelillos}	{ponce,coamo,"sabana grande",yauco,guanica}	f	'bak':7 'bizcochit':12 'don':2 'dulc':9 'lol':5 'panader':8 'pastelill':13 'postr':10 'reposter':6	'coamo':2 'grande':4 'guanica':6 'ponce':1 'sabana':3 'yauco':5	{"lun":"","mar":"no te cases","mier":"","jue":"","vier":"","sab":"los weekenes","dom":"los weekenes"}
+34	Taller de Risi	2020-11-01	https://www.youtube.com/watch?v=xOOzmR30_YY	{"fbk":"face de luisa","tt":"","igrm":"","phn":"787 827 2396"}	{34a.jpeg}	{mecanico,"grasa man",transmisiones,"deja danao"}	{"las marias",lares,"san sebastian"}	f	'dana':9 'dej':8 'gras':5 'man':6 'mecan':4 'risi':3 'tall':1 'transmision':7	'lares':3 'las':1 'marias':2 'san':4 'sebastian':5	{"lun":"hasta las 5","mar":"","mier":"","jue":"","vier":"","sab":"cerrado : soy adventista","dom":""}
+11	otro nepe de lola	2020-10-30	https://www.youtube.com/watch?v=Z3cmddZh6t8	{"fbk":"lolaEnFacebook","tt":"@lola","igrm":"@insta_lola","phn":"1234"}	{11a.jpeg,11b.jpeg}	{"book reading tutos","leemos tu libros"}	{"la uni","el colegio"}	t	'book':5 'leem':8 'libr':10 'lol':4 'nep':2 'reading':6 'tut':7	'colegio':4 'el':3 'la':1 'uni':2	{"lun":"","mar":"","mier":"","jue":"no llueve hoy","vier":"","sab":"voy sabado","dom":""}
+29	delivery don vito	2020-10-03	youtu6	{"fbk":"","tt":"@yolollevo","igrm":"","phn":""}	{29a.jpeg,29b.jpeg}	{"yo te llevo la compra"}	{"las marias",maricao,"san sebastian"}	t	'compr':8 'delivery':1 'don':2 'llev':6 'vit':3	'las':1 'marias':2 'maricao':3 'san':4 'sebastian':5	{"lun":"","mar":"","mier":"","jue":"","vier":"","sab":"weekenes","dom":"weekenes"}
 \.
 
 
@@ -214,21 +217,21 @@ COPY public.nepe (id, nombre, revisado, media_video_url, media_social_handle, me
 -- Name: bregando_bregando_id_seq; Type: SEQUENCE SET; Schema: public; Owner: victordbu
 --
 
-SELECT pg_catalog.setval('public.bregando_bregando_id_seq', 33, true);
+SELECT pg_catalog.setval('public.bregando_bregando_id_seq', 35, true);
 
 
 --
 -- Name: dueno_dueno_id_seq; Type: SEQUENCE SET; Schema: public; Owner: victordbu
 --
 
-SELECT pg_catalog.setval('public.dueno_dueno_id_seq', 14, true);
+SELECT pg_catalog.setval('public.dueno_dueno_id_seq', 18, true);
 
 
 --
 -- Name: nepe_id_seq; Type: SEQUENCE SET; Schema: public; Owner: victordbu
 --
 
-SELECT pg_catalog.setval('public.nepe_id_seq', 32, true);
+SELECT pg_catalog.setval('public.nepe_id_seq', 34, true);
 
 
 --
