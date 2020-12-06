1644c1b3298eeef8b95335844f5377644e9b82d9
vjperez <vijapefi@gmail.com>
2020-12-06 18:50:54 -0400

    valores

--- escritos/conecta/datosConectar.php
+++ escritos/conecta/datosConectar.php
@@ -4,9 +4,9 @@
 	$port = 'port=5432';
 	$dbname = 'dbname=donpapo';
 	$usuario = 'user=victordbu password=kt';
-	*/
+*/
 	define("HOST", "host=localhost");
 	define("PORT", " port=5432");
-	define("DBNAME",  " dbname=nepeype");
-	define("USUARIO", " user=victordbu password=kt");
+	define("DBNAME",  " dbname=xyz");
+	define("USUARIO", " user=abcd password=ab");
 ?>
