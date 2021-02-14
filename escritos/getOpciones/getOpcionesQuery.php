e91ac0b2a03e706b9800cbc5ddfa3bf501203ab7
victor j. perez <vijapefi@gmail.com>
2021-02-14 12:15:50 -0400

    :* concatenation for ts query

--- escritos/getOpciones/getOpcionesQuery.php
+++ escritos/getOpciones/getOpcionesQuery.php
@@ -1,14 +1,14 @@
 <?php
 //saca los valores de GET
-$queLiteralStr   = str_replace("%", ":* | ", $_GET['que']);   //here 'que' and 'donde' come as STRINGS with ':' as delimiters between words, delimeter is changed to " | ", an OR for ts query     :* means begins with on a ts query, add it to every word not just the last as would be if only added on the switch below 
-$dondeLiteralStr = str_replace("%", ":* | ", $_GET['donde']); //here 'que' and 'donde' come as STRINGS with ':' as delimiters between words, delimeter is changed to " | ", an OR for ts query.    :* means begins with on a ts query, add it to every word not just the last as would be if only added on the switch below
+$queLiteralStr   = str_replace("%", ":* | ", $_GET['que'])   . ':*';   //here 'que' and 'donde' come as STRINGS with ':' as delimiters between words, delimeter is changed to " | ", an OR for ts query     :* means begins with on a ts query, add it to every word not just the last as would be if only added on the switch below 
+$dondeLiteralStr = str_replace("%", ":* | ", $_GET['donde']) . ':*';   //here 'que' and 'donde' come as STRINGS with ':' as delimiters between words, delimeter is changed to " | ", an OR for ts query.    :* means begins with on a ts query, add it to every word not just the last as would be if only added on the switch below
 //str_replace("world","Peter","Hello world!");   produces "Hello Peter!"
 
 
 //define buscaMode
-if(strlen($queLiteralStr) == 0 && strlen($dondeLiteralStr) >  0)     $buscaMode = 'buscaDonde';
-elseif(strlen($queLiteralStr) >  0 && strlen($dondeLiteralStr) == 0) $buscaMode = 'buscaQue';
-elseif(strlen($queLiteralStr) >  0 && strlen($dondeLiteralStr) >  0) $buscaMode = 'buscaBoth';
+if(strlen($queLiteralStr) == strlen(':*') && strlen($dondeLiteralStr) >  strlen(':*'))     $buscaMode = 'buscaDonde';
+elseif(strlen($queLiteralStr) >  strlen(':*') && strlen($dondeLiteralStr) == strlen(':*')) $buscaMode = 'buscaQue';
+elseif(strlen($queLiteralStr) >  strlen(':*') && strlen($dondeLiteralStr) >  strlen(':*')) $buscaMode = 'buscaBoth';
 else throw new Exception('No tengo un Busca Mode, en getOpcionesQuery.php : ' . __FILE__ ); // sholud not get here ; throw warning
 
 //select query to be used based on $buscaMode
@@ -23,25 +23,25 @@ switch($buscaMode){
 		ORDER BY ranqueo DESC
 		";
 		pg_prepare($cnx, "preparo", $query);
-		$recurso = pg_execute($cnx, "preparo", array("$queLiteralStr:*"));
+		$recurso = pg_execute($cnx, "preparo", array($queLiteralStr));
 		break;
 	case 'buscaDonde':
 		$query = "SELECT id, array_to_json(media_foto_url), ts_rank_cd(donde_vector, el_query) AS ranqueo
-		FROM nepe, to_tsquery('simple', $1) el_query
+		FROM nepe, to_tsquery('spanish', $1) el_query
 		WHERE el_query @@ donde_vector
 		ORDER BY ranqueo DESC
 		";
 		pg_prepare($cnx, "preparo", $query);
-		$recurso = pg_execute($cnx, "preparo", array("$dondeLiteralStr:*"));
+		$recurso = pg_execute($cnx, "preparo", array($dondeLiteralStr));
 		break;
 	case 'buscaBoth':
 		$query = "SELECT id, array_to_json(media_foto_url), ts_rank_cd(nombre_que_vector, que_query) + ts_rank_cd(donde_vector, donde_query) AS ranqueo
-		FROM nepe, to_tsquery('spanish', $1) que_query,  to_tsquery('simple', $2) donde_query
+		FROM nepe, to_tsquery('spanish', $1) que_query,  to_tsquery('spanish', $2) donde_query
 		WHERE que_query @@ nombre_que_vector AND donde_query @@ donde_vector 
 		ORDER BY ranqueo DESC
 		";
 		pg_prepare($cnx, "preparo", $query);
-		$recurso = pg_execute($cnx, "preparo", array("$queLiteralStr:*", "$dondeLiteralStr:*"));
+		$recurso = pg_execute($cnx, "preparo", array($queLiteralStr, $dondeLiteralStr));
 		break;
 }
 ?>
