06dd797f49346cae6c535d35162ceb691a93e58c
victor j. perez <vijapefi@gmail.com>
2022-02-27 10:50:14 -0400

    crea dueno

---
+++ js/creaDueno.js
@@ -0,0 +1,54 @@
+//erase feedback when user writting
+jQuery('form#creaDuenoForm  input[name^=password]').keyup(function(){
+    jQuery.feedback('form#creaDuenoForm h3', '');
+});
+//erase feedback when user writting
+jQuery('form#creaDuenoForm  input[name=username]').keyup(function(){
+    jQuery.feedback('form#creaDuenoForm h3', '');
+});	
+
+
+
+
+
+
+jQuery('form#creaDuenoForm').submit(function(evento){
+    evento.preventDefault(); //not making a submit (POST request) from html action
+    var usertb = jQuery('#usernameId').val();
+    var pass01 = jQuery('#passwordId').val();
+    var pass02 = jQuery('#passwordConfirmId').val();
+    if( jQuery.areValidUserYPass(usertb, pass01, pass02, 'fullFeedback', 'form#creaDuenoForm h3') ){
+        //Valid values son los q cumplen estas 3 cosas.
+        //Estas cosas se pueden chequear antes del post y evito post sin sentido
+        // 1)lenght >= 4; 2)only numbers or letters; 3)both pass are equal;
+        //Si tengo valores q fueron registrables entonces, Making a submit (POST request) here. Not in look=editDuenoShowEmpresas
+        jQuery.post('escritos/creaDueno.php', {usertb:usertb, pass01:pass01} )//check here that password are equal
+        .done(function(datosJSONStr, estatusForDONE, xhrObjetoForDONE){
+            //el getJSON no entra al .done y cae en .fail si detecta errores de parseo.
+            //Con el post tengo yo que usar un try block para detectar errores de parseo y mandarlo a jQuery fallas
+            try{
+                //alert('datosJSONStr: ' + datosJSONStr);
+                datosJSObj = JSON.parse(datosJSONStr);
+                //alert('datosJSObj.registrado: ' + datosJSObj.registrado + '\ndatosJSObj.feedback: ' + datosJSObj.feedback + '\ndatosJSObj.duenoId: ' + datosJSObj.duenoId);
+            }catch(errorParseo){
+                var datosJSONStrAsXHRTexto = datosJSONStr;
+                var textoEstatus = 'Error parseando la siguiente respuesta del server en escritorios/creaDueno.php :<br> Mensaje: ' + errorParseo.message;
+                var elError = errorParseo.name;
+                
+                var path = jQuery.encodeAndGetErrorPath(datosJSONStrAsXHRTexto, textoEstatus, elError); // first arg is not xhr Object, so no responseText member will be obtained in encodeAndGetErrorPath() at functiones.js - will produce an undefined
+                jQuery(window.location).attr('href', path); 				
+            }
+            if(datosJSObj.registrado){
+                jQuery(window.location).attr('href', window.location.pathname + '?look=home');
+                jQuery(window.location).attr('href', window.location.pathname + '?look=home&duenoId=' + datosJSObj.duenoId);
+            }else{ // usuario es repetido en el database, por eso se chequea despues del post
+                jQuery.feedback('form#creaDuenoForm h3', datosJSObj.feedback);
+            }
+        })
+        .fail(function(xhrObjetoForFAIL, textoEstatus, elError){
+            var xhrObjetoForFAILTexto = xhrObjetoForFAIL.responseText;
+            var path = jQuery.encodeAndGetErrorPath(xhrObjetoForFAILTexto, textoEstatus, elError);
+            jQuery(window.location).attr('href', path); 
+        });
+    }
+});
