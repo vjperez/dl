e166e172ea7913c472cfaab9730f42b7e8661bcd
victor j perez <vijapefi@gmail.com>
2022-03-20 08:48:21 -0700

    limpiando git

--- escritos/conecta/datosConectar.php
+++
@@ -1,6 +0,0 @@
-<?php
-	define("HOST", "host=localhost");
-	define("PORT", " port=5432");
-	define("DBNAME",  " dbname=donlibre_nepeype");
-	define("USUARIO", " user=donlibre_victordbu password=wESt16#C%");
-?>
