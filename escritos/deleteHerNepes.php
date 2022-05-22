731ea34de6601a8eea9854a88fe60db095b0244f
victor j. perez <vijapefi@gmail.com>
2022-05-22 14:51:32 -0400

    delete her nepes escritos

--- escritos/deleteHerNepes.php
+++ escritos/deleteHerNepes.php
@@ -7,24 +7,30 @@ if(isset($_SESSION['dueno_id'])){
 		require_once 'conecta/conecta.php';
 		//i am sure i have a connection, because an exception was NOT thrown at conecta
 
-		require_once 'deleteNepes/deleteNepesQuery.php';
+		require_once 'deleteHerNepes/getHerNepeIdsQuery.php';
 		if($recurso){
             $cuantos = 0;
-			while($nepe_to_delete = pg_fetch_row($recurso)[0] ){
-				
+			while($nepe_to_delete = pg_fetch_row($recurso)[0] ){ 
                 $deleteNepeQuery = "DELETE 
                 FROM nepe
                 WHERE id = '$nepe_to_delete'";
                 pg_query($cnx, $deleteNepeQuery);
-
                 $cuantos++;
+
+				// delete foto files now that nepe on db was deleted
+				require_once 'configConstants/constants.php';
+				$fotoTarget = $fotos_subidas_dir . $nepe_to_delete . '[abcde].';
+				foreach(glob($fotoTarget . '*') as $fotoToErase){
+					if(file_exists ($fotoToErase)) unlink($fotoToErase);
+				}
+				//
 			}
             $respuesta = json_decode('{"nepesBorrados":' . $cuantos . '}');
 			pg_close($cnx);
 			echo json_encode($respuesta); 
 		}else{
 			pg_close($cnx); //maybe not needed but doesn't hurt
-			throw new Exception('Mal query.  Sin RECURSO para deleteNepesQuery en: ' . __FILE__  );
+			throw new Exception('Mal query.  Sin RECURSO para getHerNepeIdsQuery en: ' . __FILE__  );
 		}
 }else{
 	throw new Exception('Session no seteada en: ' . __FILE__  );
--- escritos/deleteHerNepes/getHerNepeIdsQuery.php
+++ escritos/deleteHerNepes/getHerNepeIdsQuery.php
@@ -1,12 +1,12 @@
 <?php
 
-$deleteNepesQuery = "SELECT 
+$getHerNepeIdsQuery = "SELECT 
 nepe.id
 FROM dueno 
 INNER JOIN  bregando ON dueno.id = bregando.dueno_id 
 INNER JOIN nepe  ON nepe.id = bregando.nepe_id
 WHERE dueno.id = '$user_to_leave_without_nepes'";
 
-$recurso = pg_query($cnx, $deleteNepesQuery);
+$recurso = pg_query($cnx, $getHerNepeIdsQuery);
 
 ?>
