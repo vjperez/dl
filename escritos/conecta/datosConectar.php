e54fe229015da20a35f4704747a41dbee4591e67
victor j. perez <vijapefi@gmail.com>
2022-06-26 17:31:17 -0400

    datos y constantes

---
+++ escritos/conecta/datosConectar.php
@@ -0,0 +1,6 @@
+<?php
+	define("HOST", "host=localhost");
+	define("PORT", " port=5432");
+	define("DBNAME",  " dbname=xxx");
+	define("USUARIO", " user=xxx password=xxx");
+?>
---
+++ escritos/configConstants/constants.php
@@ -0,0 +1,11 @@
+<?php
+define("MAQUINA_PATH", "C:/Users/victor/lighttpd/htdocs");
+define("SITE_PATH_APPEND", "/WebDevelopmentStuff");
+define("UPLOAD_DIR", "/imagenes/profile/subidas/");
+
+//define("MAQUINA_PATH", "/srv/http/proyectos");
+//define("SITE_PATH_APPEND", "/web-development/mio");
+//define("UPLOAD_DIR", "/imagenes/profile/subidas/");
+
+$fotos_subidas_dir = MAQUINA_PATH . SITE_PATH_APPEND . UPLOAD_DIR;
+?>
