b91fccd7bbbdc48d1f266fa207586884528dd494
victor j. perez <vijapefi@gmail.com>
2022-07-17 19:13:13 -0400

    delimeter commet

--- js/busca.js
+++ js/busca.js
@@ -6,9 +6,9 @@ jQuery('form').submit(function(evento){
     evento.preventDefault(); //not making a submit (GET request) here. Lets do it at look=opciones
     var regexp = new RegExp(/[^a-z0-9ñüàáèéìíòóùú+]/gi);	//see 'negated or complemented character class'
     var que = jQuery('#queId').val();
-    que = jQuery.cleanStr(que, regexp); // clean function returns cleaned str,  adds ':' as delimiter
+    que = jQuery.cleanStr(que, regexp); // clean function returns cleaned str,  adds ' ' as delimiter
     var donde = jQuery('#dondeId').val();
-    donde = jQuery.cleanStr(donde, regexp); // clean function returns cleaned str, adds  ':' as delimiter
+    donde = jQuery.cleanStr(donde, regexp); // clean function returns cleaned str, adds  ' ' as delimiter
     //alert('que=(' + que  + ')\ndonde=(' +  donde + ')');
     if(que.length > 0 || donde.length > 0){//i'm looking for a non empty cleaned str
         jQuery(window.location).attr('href', window.location.pathname + '?look=opciones&que=' + encodeURIComponent(que) 
@@ -18,4 +18,4 @@ jQuery('form').submit(function(evento){
     }
 });
 
-//}
+//}       
