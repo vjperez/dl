7322d8e7b8576bace7b1a29ac6ee4f6fb0eecc93
victor j. perez <vijapefi@gmail.com>
2022-07-31 23:26:08 -0400

    feedback warn h5

--- js/funciones-nepe.js
+++ js/funciones-nepe.js
@@ -5,12 +5,12 @@ jQuery.haveAtLeast1Red = function(formaStr){
 	&&  jQuery.isVacioStr(jQuery.cleanStr(jQuery(formaStr + ' input[name=red2]').val(), regexp))
 	&&  jQuery.isVacioStr(jQuery.cleanStr(jQuery(formaStr + ' input[name=red3]').val(), regexp)) 
 	&&  jQuery.isVacioStr(jQuery.cleanStr(jQuery(formaStr + ' input[name=red4]').val(), regexp)) ) {	
-			jQuery.feedback('fieldset#socialHandleFieldset h3', 'Minimo 1 contacto');
-			jQuery.feedback('fieldset#submitButtonFieldset h3#handlesFeedback', 'Verifica secci\u00F3n : QUIEN');
+			jQuery.feedback('fieldset#socialHandleFieldset h5', 'Minimo 1 contacto');
+			jQuery.feedback('fieldset#submitButtonFieldset h5#handlesFeedback', 'Verifica secci\u00F3n : QUIEN');
 			submitVote1 = false;
 	}else{
-			jQuery.feedback('fieldset#socialHandleFieldset h3', '');
-			jQuery.feedback('fieldset#submitButtonFieldset h3#handlesFeedback', '');
+			jQuery.feedback('fieldset#socialHandleFieldset h5', '');
+			jQuery.feedback('fieldset#submitButtonFieldset h5#handlesFeedback', '');
 			submitVote1 = true;
 	}
 }
@@ -26,29 +26,29 @@ jQuery.have5OrLessImages = function(formaStr){ //2 questions here 1) five or les
 	// question 1
 	//alert("cuantas fotos " + jQuery(formaStr + ' input#fotosId')[0].files.length);
 	if(jQuery(formaStr + ' input#fotosId')[0].files.length >= 1 && jQuery(formaStr + ' input#fotosId')[0].files.length <= 5){
-		jQuery.feedback('fieldset#fotoSrcFieldset h3#max5min1Feedback', '');
+		jQuery.feedback('fieldset#fotoSrcFieldset h5#max5min1Feedback', '');
 		fotoSrcFieldsetAddWarningClassVote1 = false;
 	}else{
-		jQuery.feedback('fieldset#fotoSrcFieldset h3#max5min1Feedback', 'Minimo 1 foto, maximo 5');
+		jQuery.feedback('fieldset#fotoSrcFieldset h5#max5min1Feedback', 'Minimo 1 foto, maximo 5');
 		fotoSrcFieldsetAddWarningClassVote1 = true;
 	}
 	// question 2
 	if(jQuery.isNotImage(formaStr)){
-		jQuery.feedback('fieldset#fotoSrcFieldset h3#isImageFeedback', 'No es foto');
+		jQuery.feedback('fieldset#fotoSrcFieldset h5#isImageFeedback', 'No es foto');
 		fotoSrcFieldsetAddWarningClassVote2 = true;
 	}else{
-		jQuery.feedback('fieldset#fotoSrcFieldset h3#isImageFeedback', '');
+		jQuery.feedback('fieldset#fotoSrcFieldset h5#isImageFeedback', '');
 		fotoSrcFieldsetAddWarningClassVote2 = false;
 	}
 	// if warning from any question
 	if(fotoSrcFieldsetAddWarningClassVote1 || fotoSrcFieldsetAddWarningClassVote2){
-		jQuery.feedback('fieldset#submitButtonFieldset h3#fotosFeedback', 'Verifica secci\u00F3n : FOTOS');
-		jQuery.feedback('fieldset#fotoSrcFieldset h3#howManyFeedback', '');
+		jQuery.feedback('fieldset#submitButtonFieldset h5#fotosFeedback', 'Verifica secci\u00F3n : FOTOS');
+		jQuery.feedback('fieldset#fotoSrcFieldset h5#howManyFeedback', '');
 
 		submitVote2 = false;
 	}else{
-		jQuery.feedback('fieldset#submitButtonFieldset h3#fotosFeedback', '');
-		jQuery.feedback('fieldset#fotoSrcFieldset h3#howManyFeedback', 'fotos: ' + jQuery(formaStr + ' input#fotosId')[0].files.length);
+		jQuery.feedback('fieldset#submitButtonFieldset h5#fotosFeedback', '');
+		jQuery.feedback('fieldset#fotoSrcFieldset h5#howManyFeedback', 'fotos: ' + jQuery(formaStr + ' input#fotosId')[0].files.length);
 
 		jQuery.getReducedImagesArray(formaStr);
 		submitVote2 = true;
