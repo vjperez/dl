84e9602b90aa322f5beb486b7142a46b672545d9
victor j. perez <vijapefi@gmail.com>
2022-08-21 08:11:09 -0400

    set commands script

---
+++ setToken.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git remote remove web-development
+git remote add web-development https://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@github.com/vjperez/web-development
+git remote --v
+
+git push --set-upstream web-development master
