209dc05b451a6fbbb8632a424a7e43f09ee0b205
victor j. perez <vijapefi@gmail.com>
2022-11-27 08:41:40 -0400

    show nepes get ids

--- escritos/showNepesGetIds/showNepesGetIdsQuery.php
+++ escritos/showNepesGetIds/showNepesGetIdsQuery.php
@@ -1,10 +1,9 @@
 <?php
 
 $getNepeIdsQuery = "SELECT 
-nepe.id, nepe.nombre 
+    nepe.id, nepe.nombre 
 FROM dueno 
-INNER JOIN  bregando ON dueno.id = bregando.dueno_id 
-INNER JOIN nepe  ON nepe.id = bregando.nepe_id
+INNER JOIN nepe  ON nepe.dueno_id = dueno.id
 WHERE dueno.id = '$dueno_to_query'";
 
 ?>
