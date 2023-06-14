726897f59ce407b22736a9a4ef5cea65671d0904
victor j. perez <vijapefi@gmail.com>
2023-06-14 15:24:07 -0400

    no jquery

--- setToken.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-git remote remove web-development
-git remote add web-development https://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@github.com/vjperez/web-development
-git remote --v
-
-git push --set-upstream web-development master
