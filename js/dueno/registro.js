a856b5be1993c7b92943754fe9b9a6d5511a9734
victor j. perez <vijapefi@gmail.com>
2023-06-29 10:50:39 -0400

    registro

--- js/dueno/registro.js
+++ js/dueno/registro.js
@@ -1,33 +1,23 @@
 jQuery('#footer').css('visibility','visible');
 
+
 //erase feedback when user writting
-jQuery('form#creaDuenoForm  input[name^=password]').keydown(function(){
-    jQuery.feedback('form#creaDuenoForm h3', '');
+jQuery('form#registroForm  input[name^=password],  form#registroForm  input[name=username]').keydown(function(){
+    jQuery.feedback('form#registroForm h3.feedback', '');
 });
-//erase feedback when user writting
-jQuery('form#creaDuenoForm  input[name=username]').keydown(function(){
-    jQuery.feedback('form#creaDuenoForm h3', '');
-});	
 
 
-
-
-
-
-jQuery('form#creaDuenoForm').submit(function(evento){
+jQuery('form#registroForm').submit(function(evento){
     evento.preventDefault(); //not making a submit (POST request) from html action
     var usertb = jQuery('#usernameId').val();
     var pass01 = jQuery('#passwordId').val();
     var pass02 = jQuery('#passwordConfirmId').val();
-    if( jQuery.areValidUserYPass(usertb, pass01, pass02, 'fullFeedback', 'form#creaDuenoForm h3') ){
-        //Valid values son los q cumplen estas 3 cosas.
-        //Estas cosas se pueden chequear antes del post y evito post sin sentido
-        // 1)lenght >= 4; 2)only numbers or letters; 3)both pass are equal;
-        //Si tengo valores q fueron registrables entonces, Making a submit (POST request) here. Not in look=editDuenoShowEmpresas
+    if( jQuery.areValidUserYPass(usertb, pass01, pass02, 'fullFeedback', 'form#registroForm h3.feedback') ){
+        //Making a submit (POST request) here. Not in look=editDuenoShowEmpresas
         jQuery.post('escritos/dueno/creaDueno.php', {usertb:usertb, pass01:pass01} )//check here that password are equal
         .done(function(datosJSONStr, estatusForDONE, xhrObjetoForDONE){
             //el getJSON no entra al .done y cae en .fail si detecta errores de parseo.
-            //Con el post tengo yo que usar un try block para detectar errores de parseo y mandarlo a jQuery fallas
+            //Con el post tengo yo que usar un try block para detectar errores de parseo
             try{
                 //alert('datosJSONStr: ' + datosJSONStr);
                 datosJSObj = JSON.parse(datosJSONStr);
@@ -37,13 +27,13 @@ jQuery('form#creaDuenoForm').submit(function(evento){
                 var textoEstatus = 'Error parseando la siguiente respuesta del server en escritorios/dueno/creaDueno.php :<br> Mensaje: ' + errorParseo.message;
                 var elError = errorParseo.name;
                 
-                var path = jQuery.encodeAndGetErrorPath(datosJSONStrAsXHRTexto, textoEstatus, elError); // first arg is not xhr Object, so no responseText member will be obtained in encodeAndGetErrorPath() at functiones.js - will produce an undefined
+                var path = jQuery.encodeAndGetErrorPath(datosJSONStrAsXHRTexto, textoEstatus, elError); 
                 jQuery(window.location).attr('href', path); 				
             }
             if(datosJSObj.registrado){
                 jQuery(window.location).attr('href', window.location.pathname + '?look=home');
             }else{ // usuario es repetido en el database, por eso se chequea despues del post
-                jQuery.feedback('form#creaDuenoForm h3', datosJSObj.feedback);
+                jQuery.feedback('form#registroForm h3.feedback', datosJSObj.feedback);
             }
         })
         .fail(function(xhrObjetoForFAIL, textoEstatus, elError){
