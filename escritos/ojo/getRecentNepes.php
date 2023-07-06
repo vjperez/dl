db9a23bd5630847b1795a12a87be486019b9ac81
victor j. perez <vijapefi@gmail.com>
2023-07-06 05:19:48 -0400

    recent fotos

--- escritos/ojo/getRecentNepes.php
+++ escritos/ojo/getRecentNepes.php
@@ -1,31 +1,45 @@
 <?php 
+require_once '../../escritos/configConstants/constants.php';
+
 //conecta al db
-require_once 'conecta/conecta.php';
+require_once  MAQUINA_PATH . SITE_PATH_APPEND  . 'escritos/conecta/conecta.php';
 //i am sure i have a connection, because an exception was NOT thrown at conecta
 
 require_once 'getRecentNepes/getRecentNepesQuery.php';
 $recurso = pg_query($cnx, $getRecentNepesQuery);
 
 if($recurso){
-	$nepe =  array();
 	$nepes = array();
     $index = 0;
     while($nepe = pg_fetch_row($recurso) ){
-        //deal with random foto first
-        $fotos = json_decode($nepe[2]);  // todas las fotos de un nepe changed from json to php array
-        $randomIndex = rand(0, -1 + count($fotos));
-        $randomNepeFoto = $fotos[$randomIndex];
-        //foto to put as fotoName has been chosen
-        $nepes[$index]['nepeId'] = json_decode($nepe[0]);
-        $nepes[$index]['nepeNombre'] = $nepe[1];
-        $nepes[$index]['nepeFotoName'] = $randomNepeFoto;
-        $nepes[$index]['dias'] = json_decode($nepe[3]);
-        $index++;
+		$nepeId = json_decode($nepe[0]);
+		$randomNepeFoto;
+        
+		require_once 'getFoto/getFotoQuery.php';
+		$recurso = pg_execute($cnx, "preparadoQueryGetFoto", array($nepeId));
+		if($recurso){
+			$fotos = array();
+			$fotoIndex = 0;
+			while($foto = pg_fetch_row($recurso)) {
+				$fotos[$fotoIndex] = $foto[0];    
+				$fotoIndex++;
+			}
+			$randomIndex = rand(0, -1 + count($fotos));
+			$randomNepeFoto = $fotos[$randomIndex];
+		}else{
+		    pg_close($cnx); 
+			throw new Exception('Mal query.  Sin RECURSO para preparadoQueryGetFoto en: ' . __FILE__  );	
+		}
+		$nepes[$index]['nepeId'] = $nepeId;
+		$nepes[$index]['nepeNombre'] = $nepe[1];
+		$nepes[$index]['dias'] = json_decode($nepe[2]);
+		$nepes[$index]['nepeFotoName'] = $randomNepeFoto;
+		$index++;
     }
     pg_close($cnx);
     echo json_encode($nepes);
 }else{
-    pg_close($cnx); //maybe not needed but doesn't hurt
-    throw new Exception('Mal query.  Sin RECURSO en: ' . __FILE__  );
+    pg_close($cnx);
+    throw new Exception('Mal query.  Sin RECURSO para getRecentNepesQuery en: ' . __FILE__  );
 }
 ?>
--- escritos/ojo/getRecentNepes/getRecentNepesQuery.php
+++ escritos/ojo/getRecentNepes/getRecentNepesQuery.php
@@ -1,9 +1,9 @@
 <?php
-$intervalo_en_dias = 1 * 365;
+$intervalo_en_dias = 14;
 $getRecentNepesQuery = "SELECT
-    nepe.id, nepe.nombre, array_to_json(nepe.media_foto_url), (now()::date - nepe.revisado) as dias 
+    nepe.id, nepe.nombre, (now()::date - nepe.creado) as dias 
 FROM nepe
 
-WHERE (now()::date - nepe.revisado) < $intervalo_en_dias 
+WHERE (now()::date - nepe.creado) < $intervalo_en_dias 
 order by dias asc";
 ?>
