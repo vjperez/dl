8f716b8170019a8bd1c1ab7e240697ce471df6ac
victor j. perez <vijapefi@gmail.com>
2023-07-11 11:09:48 -0400

    query to get user nepes

--- escritos/dueno/read/nepesWithIdsQuery.php
+++ escritos/dueno/read/nepesWithIdsQuery.php
@@ -3,7 +3,8 @@
 $queryNepesWithIds = "SELECT 
     nepe.id, nepe.nombre 
 FROM dueno 
-INNER JOIN nepe  ON nepe.dueno_id = dueno.id
+INNER JOIN dueno_nepe  ON  dueno_nepe.dueno_id = dueno.id
+INNER JOIN nepe        ON  dueno_nepe.nepe_id  =  nepe.id
 WHERE dueno.id = $1";
 
 pg_prepare($cnx, "preparadoQueryNepesWithIds", $queryNepesWithIds);
