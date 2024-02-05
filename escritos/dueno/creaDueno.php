375f695fef5204cb95dcbafa18a8c8dcfd4d6223
victor j. perez <vijapefi@gmail.com>
2024-02-05 17:34:29 -0400

    pg prepare

--- escritos/dueno/creaDueno.php
+++ escritos/dueno/creaDueno.php
@@ -5,14 +5,12 @@ $pass01 = $_POST['pass01'];
 $hashed_pass01 = password_hash($pass01 , PASSWORD_DEFAULT);
 
 //conecta al db
-require_once 'escritos/conecta/conecta.php';
+require_once '../conecta/conecta.php';
 //i am sure i have a connection, because an exception was NOT thrown at conecta
 
-
-require_once 'escritos/dueno/read/idQuery.php';
-$recurso = pg_execute($cnx, "preparadoQueryId", array($usertb));
+require_once '../dueno/read/idQuery.php';
+$recurso = pg_execute($cnx, "preparadoQueryGetIdFromNombre", array($usertb));
 if($recurso){
-	///////////////////////////////////////////////////////
 	//if i can fetch a db row with a user id ... i know the usertb is NOT a new username
 	//if there is no row, pg fetch row returns FALSE  ...  which means the username is new
 	$isNewUsername;
@@ -23,28 +21,28 @@ if($recurso){
 	}
 	///////////////////////////////////////////////////////
 	if($isNewUsername){
-		require_once 'dueno/crea/insertQuery.php';
+		require_once '../dueno/crea/insertQuery.php';
 		$recurso = pg_execute($cnx, "preparadoQueryInsert", array($usertb, $hashed_pass01));
 		if($recurso){
 			$recurso = pg_query($cnx, "SELECT currval('dueno_id_seq')"); //otro recurso, ahora con fila q tiene id recien insertado
 			$filaConId = pg_fetch_row ($recurso);
 			$dueno_id = $filaConId[0];
-			$respuesta = json_decode('{"registrado":true, "feedback":"Ya estas registrado.  Directo a mi cuenta, no uso feedback."}');
+			session_start();	$_SESSION['dueno_id'] = $dueno_id;
 			pg_close($cnx);
-			echo json_encode ($respuesta);
-			session_start();	$_SESSION['dueno_id'] = $dueno_id; 
+			$respuesta = json_decode('{"registrado":true, "feedback":"Ya estas registrado.  Directo a mi cuenta, no uso feedback."}');
+			echo json_encode ($respuesta); 
 		}else{
 			pg_close($cnx);
-			throw new Exception('Mal query. Sin RECURSO, para preparadoQueryInsert. (username es nuevo ... pero, user not inserted, en: )' . __FILE__ );
+			throw new Exception('Mal query. Sin RECURSO, para preparadoQueryInsert. Username es nuevo, pero ... user not inserted, en: ' . __FILE__ );
 		}
 	}else{
-		$respuesta = json_decode('{"registrado":false, "feedback":"Username no disponible."}');
+		$respuesta = json_decode('{"registrado":false, "feedback":"Username no disponible, ya esta usado."}');
 		pg_close($cnx);
 		echo json_encode ($respuesta);
 	}
 	///////////////////////////////////////////////////////
 }else{
 	pg_close($cnx);
-	throw new Exception('Mal query.  Sin RECURSO, para preparadoQueryId.  (Ni se chequio el username.) en: ' . __FILE__ );
+	throw new Exception('Mal query.  Sin RECURSO, para preparadoQueryGetIdFromNombre.  (No se pudo verificar si el nombre ya tiene id.) en: ' . __FILE__ );
 }
 ?>
--- escritos/dueno/read/idQuery.php
+++ escritos/dueno/read/idQuery.php
@@ -1,9 +1,9 @@
 <?php
-//query to get user id using a username
+//query to get user id using a 'nombre'
 $queryId = "SELECT 
 	id
 FROM dueno
 WHERE nombre = $1";
 
-pg_prepare($cnx, "preparadoQueryId", $queryId);
+pg_prepare($cnx, "preparadoQueryGetIdFromNombre", $queryId);
 ?>
