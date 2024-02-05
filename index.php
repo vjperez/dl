2c058f6d5ef8a3e8ef33f35be12ab21cfcfe64d5
victor j. perez <vijapefi@gmail.com>
2024-02-05 19:06:59 -0400

    config constants from get recent nepes

--- index.php
+++ index.php
@@ -1,7 +1,4 @@
 <?php
-//defines some required constants
-require_once 'escritos/configConstants/constants.php';
-
 //header('Location: portada.html?look=busca' , true , 301);  // permanente
 header('Location: portada.html' , true , 301);  // permanente
 exit;
