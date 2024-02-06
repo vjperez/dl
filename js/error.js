78b8f15294db3d60221efad0fa91fb40975f10b8
victor j. perez <vijapefi@gmail.com>
2024-02-06 10:51:13 -0400

    error view

--- js/error.js
+++ js/error.js
@@ -1,9 +1,9 @@
 jQuery.appendDebugErrors = function( xhrObjetoForFAILString, textoEstatus, elError ){
     losLis = '<br><hr>';
         
-    losLis += '<li><span class="color01enfasis">El error:<br></span>' + elError + '</b><br><br></li>';		
-    losLis += '<li><span class="color01enfasis">Texto Estatus:<br></span>' + textoEstatus + '<br><br></li>';
-    losLis += '<li><span class="color01enfasis">xhr Objecto Stringificado:<br></span>'      + xhrObjetoForFAILString + '</li>';
+    losLis += '<li><span class="color01enfasis">El error:<br></span>' + elError + '<br><br></li>';		
+    losLis += '<li><span class="color01enfasis">Texto Estatus:<br></span>' + textoEstatus + '</b><br><br></li>';
+    losLis += '<li><span class="color01enfasis">xhr Objecto Stringificado o Texto devuelto:<br></span>'      + xhrObjetoForFAILString + '</li>';
         
     losLis += '<br><hr>';
     jQuery('#containerForErrors').append(losLis);
