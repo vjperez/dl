efa3b679faf7f3b850e2aeb40356627a2d7158ae
victor j. perez <vijapefi@gmail.com>
2024-02-06 09:36:29 -0400

    login registro js

--- js/dueno/login.js
+++ js/dueno/login.js
@@ -1,13 +1,8 @@
-//when ajax complete ; handle form submit and make post
 jQuery('form#loginForm').submit(function(evento){
 	evento.preventDefault(); //not making a submit (POST request) from html action.
 	var user = jQuery('#usernameId').val();
 	var pass = jQuery('#passwordId').val();
 	if( jQuery.areValidUserYPass(user, pass, pass, "genericFeedback", 'form#loginForm h3') ){
-		//Valid values son los q cumplen estas 3 cosas.
-		//Estas cosas se pueden chequear antes del post y evito post sin sentido
-		// 1)lenght >= 4; 2)only numbers or letters; 3)both pass are equal;
-		//Si tengo valores q fueron registrables entonces, Making a submit (POST request) here. Not in look=editDuenoShowEmpresas
 		jQuery.post('escritos/dueno/login.php', {user:user, pass:pass} )
 		.done(function(datosJSONStr, estatusForDONE, xhrObjetoForDONE){
 			//el getJSON no entra al .done y cae en .fail si detecta errores de parseo.
@@ -15,7 +10,6 @@ jQuery('form#loginForm').submit(function(evento){
 			try{
 				//alert('datosJSONStr: ' + datosJSONStr);
 				datosJSObj = JSON.parse(datosJSONStr);
-				//alert('datosJSObj.logueado: ' + datosJSObj.logueado);
 			}catch(errorParseo){
 				var datosJSONStrAsXHRTexto = datosJSONStr;
 				var textoEstatus = 'Error parseando la siguiente respuesta del server desde escritos/dueno/login.php :<br> Mensaje: ' + errorParseo.message;
@@ -25,10 +19,8 @@ jQuery('form#loginForm').submit(function(evento){
 				jQuery(window.location).attr('href', path); 
 			}
 			if(datosJSObj.logueado){
-				//jQuery(window.location).attr('href', window.location.pathname + '?look=home&duenoId=' + datosJSObj.duenoId);
 				jQuery(window.location).attr('href', window.location.pathname + '?look=home');
 			}else{
-				//alert('datosJSObj.logueado: ' + datosJSObj.logueado);
 				jQuery.feedback('form#loginForm h3', 'Trata otra vez.');
 			}
 		})
@@ -40,16 +32,9 @@ jQuery('form#loginForm').submit(function(evento){
 	}
 });
 
-
-
-//erase feedback when user writting
-jQuery('form#loginForm  input[name=password]').keydown(function(){
-	jQuery.feedback('form#loginForm h3', '');
+//erase feedback when user writes
+jQuery('form[id*=Form]  input[name^=password],  form[id*=Form]  input[name=username]').keydown(function(){
+	jQuery.feedback('form[id*=Form] h3', '');
 });
-//erase feedback when user writting
-jQuery('form#loginForm  input[name=username]').keydown(function(){
-	jQuery.feedback('form#loginForm h3', '');
-});
-
 
 jQuery('#footer').css('visibility','visible');
--- js/dueno/registro.js
+++ js/dueno/registro.js
@@ -1,28 +1,17 @@
-jQuery('#footer').css('visibility','visible');
-
-
-//erase feedback when user writting
-jQuery('form#registroForm  input[name^=password],  form#registroForm  input[name=username]').keydown(function(){
-    jQuery.feedback('form#registroForm h3.feedback', '');
-});
-
-
 jQuery('form#registroForm').submit(function(evento){
     evento.preventDefault(); //not making a submit (POST request) from html action
     var usertb = jQuery('#usernameId').val();
     var pass01 = jQuery('#passwordId').val();
     var pass02 = jQuery('#passwordConfirmId').val();
     if( jQuery.areValidUserYPass(usertb, pass01, pass02, 'fullFeedback', 'form#registroForm h3.feedback') ){
-        //Making a submit (POST request) here. Not in look=editDuenoShowEmpresas
         jQuery.post('escritos/dueno/creaDueno.php', {usertb:usertb, pass01:pass01} )//check here that password are equal
         .done(function(datosJSONStr, estatusForDONE, xhrObjetoForDONE){
             //el getJSON no entra al .done y cae en .fail si detecta errores de parseo.
-            //Con el post tengo yo que usar un try block para detectar errores de parseo
+            //Con el post tengo yo que usar un try block para detectar errores de parseo y mandarlo a jQuery fallas
             try{
                 //alert('datosJSONStr: ' + datosJSONStr);
                 datosJSObj = JSON.parse(datosJSONStr);
             }catch(errorParseo){
-				//alert('errorParseo message: ' + errorParseo.message);
                 var datosJSONStrAsXHRTexto = datosJSONStr;
                 var textoEstatus = 'Error parseando la siguiente respuesta del server en escritorios/dueno/creaDueno.php :<br> Mensaje: ' + errorParseo.message;
                 var elError = errorParseo.name;
@@ -43,3 +32,10 @@ jQuery('form#registroForm').submit(function(evento){
         });
     }
 });
+
+//erase feedback when user writes
+jQuery('form[id*=Form]  input[name^=password],  form[id*=Form]  input[name=username]').keydown(function(){
+	jQuery.feedback('form[id*=Form] h3', '');
+});
+
+jQuery('#footer').css('visibility','visible');
