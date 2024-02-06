78d0d1c38e97ce61bf6bf9d44b9517d20b69b71d
victor j. perez <vijapefi@gmail.com>
2024-02-06 16:42:46 -0400

    improving require once on login

--- escritos/dueno/read/idAndClaveQuery.php
+++ escritos/dueno/read/idAndClaveQuery.php
@@ -1,6 +1,6 @@
 <?php
-// sacando el id y clave usando id
-//despues se setea last log usando id 
+// sacando el id y clave usando nombre
+// despues se setea last log usando id 
 
 $queryIdAndClave = "SELECT id, clave
 FROM dueno
