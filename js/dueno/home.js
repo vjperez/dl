af80614e245c096ac3e9e84b4df290e0a7d2a435
victor j. perez <vijapefi@gmail.com>
2024-02-07 15:39:23 -0400

    home label and table div

--- js/dueno/home.js
+++ js/dueno/home.js
@@ -1,108 +1,4 @@
-////////////////////////////////// //handle functions  ////////////////////////////////////////////
-//erase feedback when user writting
-jQuery('form#editClaveForm  input[name^=password]').keydown(function(){
-    jQuery.feedback('form#editClaveForm h3.feedback', '');
-    jQuery.feedback('form#editClaveForm h5.warn', '');
-});
-
-jQuery('form#editContactosForm  input[name^=red]').keydown(function(){
-    jQuery.feedback('form#editContactosForm h3.feedback', '');
-    jQuery.feedback('form#editContactosForm h5.warn', '');
-});
-
-//handle link to crea nepe when click on button
-jQuery('form#labelTableContainerForm :button').click(function(){
-    //alert(window.location.pathname + '?look=creaNepe'); 
-    jQuery(window.location).attr('href', window.location.pathname + '?look=creaNepe');
-});
-
-//handle form submit;
-//alert('.submit @ home...') 
 var usuario = "";
-jQuery('form#editClaveForm').submit(function(evento){
-    evento.preventDefault(); //not making a submit (POST request) from html action.
-    var user = 'valorDummy';
-    var pass01 = jQuery('#passwordId').val();
-    var pass02 = jQuery('#passwordConfirmId').val();
-    if( jQuery.areValidUserYPass(user, pass01, pass02, 'fullFeedback', 'form#editClaveForm h3.feedback') ){
-        jQuery.post('escritos/dueno/editClave.php', {pass01:pass01} )
-        .done(function(datosJSONStr, estatusForDONE, xhrObjetoForDONE){ 
-            //el getJSON no entra al .done y cae en .fail si detecta errores de parseo.
-            //Con el post tengo yo que usar un try block para detectar errores de parseo y mandarlo a jQuery fallas
-            try{
-                //alert('datosJSONStr: ' + datosJSONStr);
-                datosJSObj = JSON.parse(datosJSONStr);
-                //alert('datosJSObj.loguea: ' + datosJSObj.loguea);
-            }catch(errorParseo){
-                var datosJSONStrAsXHRTexto = datosJSONStr;
-                var textoEstatus = 'Error parseando la siguiente respuesta del servidor desde escritos/dueno/editClave.php :<br> Mensaje: ' + errorParseo.message;
-                var elError = errorParseo.name;
-                
-                var path = jQuery.encodeAndGetErrorPath(datosJSONStrAsXHRTexto, textoEstatus, elError);
-                jQuery(window.location).attr('href', path);			
-            }
-			
-            if(datosJSObj.editado){
-				var feedback = usuario + ' tu password fue editado.'; 
-				jQuery.feedback('form#editClaveForm h3.feedback', feedback);
-            }else{
-				var feedback = 'Trata otra vez, ' + usuario + '.';
-                jQuery.feedback('form#editClaveForm h3.feedback', feedback);
-            }
-        })
-        .fail(function(xhrObjetoForFAIL, textoEstatus, elError){
-            var xhrObjetoForFAILString = JSON.stringify(  xhrObjetoForFAIL  );
-            var path = jQuery.encodeAndGetErrorPath(xhrObjetoForFAILString, textoEstatus, elError);
-            jQuery(window.location).attr('href', path);
-        });
-    }
-}); // editClaveForm submit
-
-jQuery('form#editContactosForm').submit(function(evento){
-    evento.preventDefault(); //not making a submit (POST request) from html action.
-    var tel        = jQuery('form#editContactosForm  input#red1Id').val();
-	var email      = jQuery('form#editContactosForm  input#red2Id').val();
-    var redSocial1 = jQuery('form#editContactosForm  input#red3Id').val();
-    var redSocial2 = jQuery('form#editContactosForm  input#red4Id').val();
-	jQuery.post('escritos/dueno/bregaContactos.php', {tel:tel, email:email, redSocial1:redSocial1, redSocial2:redSocial2} )
-	.done(function(datosJSONStr, estatusForDONE, xhrObjetoForDONE){ 
-		//el getJSON no entra al .done y cae en .fail si detecta errores de parseo.
-		//Con el post tengo yo que usar un try block para detectar errores de parseo y mandarlo a jQuery fallas
-		try{
-			//alert('datosJSONStr: ' + datosJSONStr);
-			datosJSObj = JSON.parse(datosJSONStr);
-			//alert('datosJSObj.loguea: ' + datosJSObj.loguea);
-		}catch(errorParseo){
-			var datosJSONStrAsXHRTexto = datosJSONStr;
-			var textoEstatus = 'Error parseando la siguiente respuesta del servidor desde escritos/dueno/editContactos.php :<br> Mensaje: ' + errorParseo.message;
-			var elError = errorParseo.name;
-			
-			var path = jQuery.encodeAndGetErrorPath(datosJSONStrAsXHRTexto, textoEstatus, elError);
-			jQuery(window.location).attr('href', path);			
-		}
-		
-		if(datosJSObj.editados){
-			var feedback = usuario + ' tus contactos fueron editados.'; 
-			jQuery.feedback('form#editContactosForm h3.feedback', feedback);
-		}else if(datosJSObj.creados){
-			var feedback = usuario + ' tus contactos fueron creados.'; 
-			jQuery.feedback('form#editContactosForm h3.feedback', feedback);
-		}else{
-			var feedback = 'Trata otra vez, ' + usuario + '.';
-			jQuery.feedback('form#editContactosForm h3.feedback', feedback);
-		}
-	})
-	.fail(function(xhrObjetoForFAIL, textoEstatus, elError){
-		var xhrObjetoForFAILString = JSON.stringify(  xhrObjetoForFAIL  );
-		var path = jQuery.encodeAndGetErrorPath(xhrObjetoForFAILString, textoEstatus, elError);
-        jQuery(window.location).attr('href', path);
-	});
-    
-}); // editContactosForm submit
-
-////////////////////////////////////// handlers ///////////////////////////////////////////////////
-
-
 ////////////////////////////////////// ajax to populate home page /////////////////////////////////
 jQuery.ajax({
 	//cache: false,
@@ -159,6 +55,90 @@ jQuery.ajax({
 	var xhrObjetoForFAILString = JSON.stringify(  xhrObjetoForFAIL  );
     var path = jQuery.encodeAndGetErrorPath(xhrObjetoForFAILString, textoEstatus, elError);
     jQuery(window.location).attr('href', path);
+});
+
+jQuery('form#editClaveForm').submit(function(evento){
+    evento.preventDefault(); //not making a submit (POST request) from html action.
+    var user = 'valorDummy';
+    var pass01 = jQuery('#passwordId').val();
+    var pass02 = jQuery('#passwordConfirmId').val();
+    if( jQuery.areValidUserYPass(user, pass01, pass02, 'fullFeedback', 'form#editClaveForm h3.feedback') ){
+        jQuery.post('escritos/dueno/editClave.php', {pass01:pass01} )
+        .done(function(datosJSONStr, estatusForDONE, xhrObjetoForDONE){ 
+            try{
+                datosJSObj = JSON.parse(datosJSONStr);
+            }catch(errorParseo){
+                var datosJSONStrAsXHRTexto = datosJSONStr;
+                var textoEstatus = 'Error parseando la siguiente respuesta del servidor desde escritos/dueno/editClave.php :<br> Mensaje: ' + errorParseo.message;
+                var elError = errorParseo.name;
+                
+                var path = jQuery.encodeAndGetErrorPath(datosJSONStrAsXHRTexto, textoEstatus, elError);
+                jQuery(window.location).attr('href', path);			
+            }
+			
+            if(datosJSObj.editado){
+				var feedback = 'Clave de ' + usuario + ', fue editada.'; 
+				jQuery.feedback('form#editClaveForm h3.feedback', feedback);
+            }else{
+				var feedback = 'Trata otra vez, ' + usuario + '.';
+                jQuery.feedback('form#editClaveForm h3.feedback', feedback);
+            }
+        })
+        .fail(function(xhrObjetoForFAIL, textoEstatus, elError){
+            var xhrObjetoForFAILString = JSON.stringify(  xhrObjetoForFAIL  );
+            var path = jQuery.encodeAndGetErrorPath(xhrObjetoForFAILString, textoEstatus, elError);
+            jQuery(window.location).attr('href', path);
+        });
+    }
+}); // editClaveForm submit
+
+jQuery('form#editContactosForm').submit(function(evento){
+    evento.preventDefault(); //not making a submit (POST request) from html action.
+    var tel        = jQuery('form#editContactosForm  input#red1Id').val();
+	var email      = jQuery('form#editContactosForm  input#red2Id').val();
+    var redSocial1 = jQuery('form#editContactosForm  input#red3Id').val();
+    var redSocial2 = jQuery('form#editContactosForm  input#red4Id').val();
+	jQuery.post('escritos/dueno/bregaContactos.php', {tel:tel, email:email, redSocial1:redSocial1, redSocial2:redSocial2} )
+	.done(function(datosJSONStr, estatusForDONE, xhrObjetoForDONE){
+		try{
+			datosJSObj = JSON.parse(datosJSONStr);
+		}catch(errorParseo){
+			var datosJSONStrAsXHRTexto = datosJSONStr;
+			var textoEstatus = 'Error parseando la siguiente respuesta del servidor desde escritos/dueno/editContactos.php :<br> Mensaje: ' + errorParseo.message;
+			var elError = errorParseo.name;
+			
+			var path = jQuery.encodeAndGetErrorPath(datosJSONStrAsXHRTexto, textoEstatus, elError);
+			jQuery(window.location).attr('href', path);			
+		}
+		
+		if(datosJSObj.editados){
+			var feedback = usuario + ' tus contactos fueron editados.'; 
+			jQuery.feedback('form#editContactosForm h3.feedback', feedback);
+		}else if(datosJSObj.creados){
+			var feedback = usuario + ' tus contactos fueron creados.'; 
+			jQuery.feedback('form#editContactosForm h3.feedback', feedback);
+		}else{
+			var feedback = 'Trata otra vez, ' + usuario + '.';
+			jQuery.feedback('form#editContactosForm h3.feedback', feedback);
+		}
+	})
+	.fail(function(xhrObjetoForFAIL, textoEstatus, elError){
+		var xhrObjetoForFAILString = JSON.stringify(  xhrObjetoForFAIL  );
+		var path = jQuery.encodeAndGetErrorPath(xhrObjetoForFAILString, textoEstatus, elError);
+        jQuery(window.location).attr('href', path);
+	});
+    
+}); // editContactosForm submit
+
+//erase feedback when user writes
+jQuery('form[id*=Form]  input[name^=password],  form[id*=Form]  input[name^=red]').keydown(function(){
+	jQuery.feedback('form[id*=Form] h3', '');
+	jQuery.feedback('form[id*=Form] h5', '');
+});
+
+//handle link to crea nepe when click on button
+jQuery('div#labelTableContainer :button').click(function(){
+    jQuery(window.location).attr('href', window.location.pathname + '?look=creaNepe');
 });
 
 jQuery.hideThemSections();
