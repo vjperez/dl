1b01a42af194e2d81259243d1445a1b57048e1f4
victor j. perez <vijapefi@gmail.com>
2024-02-08 14:02:38 -0400

    using getScript for get recent nepes

--- js/nepe/getRecentNepes.js
+++ js/nepe/getRecentNepes.js
@@ -1,33 +1,28 @@
-jQuery.getRecentNepes = function(){	
-    
-    // task 1
-	jQuery.getJSON('escritos/ojo/getRecentNepes.php')
-	.done(function(datos, estatusForDONE, xhrObjetoForDONE){
-		var tableRows = '';
-		jQuery.each(datos, function(index){
-				tableRows += '<tr class="texto"><td>' 
-				+ '<a class="" href="portada.html?look=profile'
-				+  '&nepeId=' + datos[index].nepeId  + '">' 
-				+ datos[index].nepeNombre + '   <span class="diasOld">[' +  datos[index].dias  +  ' dias]</span>'
-				+'</a>' 
-				+ '</td></tr>'; 
+jQuery.getJSON('escritos/ojo/getRecentNepes.php')
+.done(function(datos, estatusForDONE, xhrObjetoForDONE){
+	var tableRows = '';
+	jQuery.each(datos, function(index){
+			tableRows += '<tr class="texto"><td>' 
+			+ '<a class="" href="portada.html?look=profile'
+			+  '&nepeId=' + datos[index].nepeId  + '">' 
+			+ datos[index].nepeNombre + '   <span class="diasOld">[' +  datos[index].dias  +  ' dias]</span>'
+			+'</a>' 
+			+ '</td></tr>'; 
 
-				tableRows +=  '<tr class="foto"><td>' 
-				+ '<a class="" href="portada.html?look=profile'
-				+  '&nepeId=' + datos[index].nepeId  + '">' 
-				+ '<img class="" src="imagenes/profile/subidas/' + datos[index].nepeFotoName + '">'
-				+ '</a>' 
-				+ '</td></tr>'; 
-				tableRows += '<tr><td></td></tr><tr><td></td></tr>';
-		});
-		
-		jQuery('#labelAndTableContainer table').html(tableRows);
-		jQuery('#footer').css('visibility','visible');
-	})
-	.fail(function(xhrObjetoForFAIL, textoEstatus, elError){
-		var xhrObjetoForFAILString = JSON.stringify(  xhrObjetoForFAIL  );
-        var path = jQuery.encodeAndGetErrorPath(xhrObjetoForFAILString, textoEstatus, elError);
-        jQuery(window.location).attr('href', path); 
+			tableRows +=  '<tr class="foto"><td>' 
+			+ '<a class="" href="portada.html?look=profile'
+			+  '&nepeId=' + datos[index].nepeId  + '">' 
+			+ '<img class="" src="imagenes/profile/subidas/' + datos[index].nepeFotoName + '">'
+			+ '</a>' 
+			+ '</td></tr>'; 
+			tableRows += '<tr><td></td></tr><tr><td></td></tr>';
 	});
-		
-}
+	
+	jQuery('#labelAndTableContainer table').html(tableRows);
+	jQuery('#footer').css('visibility','visible');
+})
+.fail(function(xhrObjetoForFAIL, textoEstatus, elError){
+	var xhrObjetoForFAILString = JSON.stringify(  xhrObjetoForFAIL  );
+	var path = jQuery.encodeAndGetErrorPath(xhrObjetoForFAILString, textoEstatus, elError);
+	jQuery(window.location).attr('href', path); 
+});
