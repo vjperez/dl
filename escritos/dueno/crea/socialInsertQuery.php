a64b9b5a92c7374efba0b3d3bfd501bf2eadb8bb
victor j. perez <vijapefi@gmail.com>
2024-02-09 07:50:56 -0400

    social insert query

--- escritos/dueno/crea/socialInsertQuery.php
+++ escritos/dueno/crea/socialInsertQuery.php
@@ -1,5 +1,5 @@
 <?php
-// query to insert new dueno on db
+// query to insert dueno's social red handle, using tipo and id
 $querySocialInsert = "INSERT INTO
 	social(dueno_id, tipo, handle)
 	VALUES($1, $2, $3)";
