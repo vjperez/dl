b1903879906c2a52cea4fd8d2f6a808e80a07d85
victor j. perez <vijapefi@gmail.com>
2024-02-09 15:42:13 -0400

    removing + sign from regexp on busca js

--- js/busca.js
+++ js/busca.js
@@ -1,6 +1,6 @@
 jQuery('form').submit(function(evento){
     evento.preventDefault(); //not making a submit (GET request) here. Lets do it at look=opciones
-    var regexp = new RegExp(/[^a-z0-9ñüàáèéìíòóùú+]/gi);	//see 'negated or complemented character class'
+    var regexp = new RegExp(/[^a-z0-9ñüàáèéìíòóùú]/gi);	//see 'negated or complemented character class'; basically what characters are not letter and not numbers
     var que = jQuery('#queId').val();
     que = jQuery.cleanStr(que, regexp); // clean function returns cleaned str,  adds ' ' as delimiter
     var donde = jQuery('#dondeId').val();
