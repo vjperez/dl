933e070aade50f1cf0624db6ad44c9c0df79afa4
victor j. perez <vijapefi@gmail.com>
2024-02-20 18:22:30 -0400

    js funciones for navigation

---
+++ js/navega.js
@@ -0,0 +1,28 @@
+jQuery(document).ready(function(){
+
+	let look = jQuery.urlParametro('look');
+
+	jQuery.getJSON('escritos/session/isSessionSet.php', {key:'dueno_id'})
+	.done(function(logueadoRespuesta, estatusForDONE, xhrObjetoForDONE){
+		//alert('for dueno_id ... logueadoRespuesta.isSet: ' + logueadoRespuesta.isSet);
+		let logueado = logueadoRespuesta.isSet;
+		console.log('logueado local in DONE:' + logueado);
+
+		switch(look){
+			case'busca':
+				if(!logueado)     { jQuery('#navLogin').css('visibility','visible').show();     jQuery('#navSignup').css('visibility','visible').show(); }
+				else              { jQuery('#navLogout').css('visibility','visible').show();    jQuery('#navHome').css('visibility','visible').show()  ; }
+				jQuery('#navBusca').addClass("seleccionado");
+			break;
+
+
+			case null:
+			default:
+				
+			break;
+		}
+
+	});
+
+});
+
