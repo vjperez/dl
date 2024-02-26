08bcddef1be3f9fc38ec5d58cae74a085f7983fd
victor j. perez <vijapefi@gmail.com>
2024-02-26 12:42:09 -0400

    get session value

--- escritos/session/getSessionValue.php
+++ escritos/session/getSessionValue.php
@@ -2,9 +2,9 @@
     session_start();
     
     $key = $_GET['key'];
-    //$value = false;
-    //if(isset($_SESSION[$key]))   { $value = $_SESSION[$key]; }
-    //$respuesta = json_decode('{"valor":' + $value + '}');
-    $valorSession = json_decode( $_SESSION[$key] );
-    echo json_encode( $valorSession );
+    
+    if(isset($_SESSION[$key]))   { $value = $_SESSION[$key]; }
+    else                         { $value = null; }
+    $respuesta = json_decode('{"valorSession":' + $value + '}');
+    echo json_encode( $respuesta );
 ?>
