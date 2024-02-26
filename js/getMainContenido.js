711cfdaadc2065d21af246ad13bd71989d8afe4e
victor j. perez <vijapefi@gmail.com>
2024-02-26 13:32:01 -0400

    using logueado, nepe index js variable for update nepe

--- js/getMainContenido.js
+++ js/getMainContenido.js
@@ -1,177 +1,116 @@
 jQuery(document).ready(function(){
 
-		var acto = jQuery.urlParametro('acto');
-		switch(acto){
-			case 'logout':
-				jQuery.logout();
-			break;
-			case 'deleteNepe':
-				var nepeId = jQuery.urlParametro('nepeId');
-				
-				jQuery.getJSON('escritos/deleteNepe.php', {nepeId:nepeId})
-				.done(function(datos, estatusForDONE, xhrObjetoForDONE){  
-					//alert('key: ' + key + '\ndatos.isSet: ' + datos.isSet);
-					if(datos.nepeBorrado)  {  }
-					else             {   }
-				})
-				.fail(function(xhrObjetoForFAIL, textoEstatus, elError){
-					var xhrObjetoForFAILString = JSON.stringify(  xhrObjetoForFAIL  );
-					var path = jQuery.encodeAndGetErrorPath(xhrObjetoForFAILString, textoEstatus, elError);
-					jQuery(window.location).attr('href', path);
-				});				
-			break;
-			case 'deleteHerNepes':
-				var userId = jQuery.urlParametro('userId');
-				
-				jQuery.getJSON('escritos/deleteHerNepes.php', {userId:userId})
-				.done(function(datos, estatusForDONE, xhrObjetoForDONE){  
-					//alert('key: ' + key + '\ndatos.isSet: ' + datos.isSet);
-					if(datos.nepesBorrados > 0)  {  }
-					else             {   }
-				})
-				.fail(function(xhrObjetoForFAIL, textoEstatus, elError){
-					var xhrObjetoForFAILString = JSON.stringify(  xhrObjetoForFAIL  );
-					var path = jQuery.encodeAndGetErrorPath(xhrObjetoForFAILString, textoEstatus, elError);
-					jQuery(window.location).attr('href', path);
-				});				
-			break;
-			case null:
-			default :
-			break;
-		}
+	var acto = jQuery.urlParametro('acto');
+	switch(acto){
+		case 'logout':
+			jQuery.logout();
+		break;
+		case 'deleteNepe':
+			var nepeId = jQuery.urlParametro('nepeId');
+			
+			jQuery.getJSON('escritos/deleteNepe.php', {nepeId:nepeId})
+			.done(function(datos, estatusForDONE, xhrObjetoForDONE){  
+				//alert('key: ' + key + '\ndatos.isSet: ' + datos.isSet);
+				if(datos.nepeBorrado)  {  }
+				else             {   }
+			})
+			.fail(function(xhrObjetoForFAIL, textoEstatus, elError){
+				var xhrObjetoForFAILString = JSON.stringify(  xhrObjetoForFAIL  );
+				var path = jQuery.encodeAndGetErrorPath(xhrObjetoForFAILString, textoEstatus, elError);
+				jQuery(window.location).attr('href', path);
+			});				
+		break;
+		case 'deleteHerNepes':
+			var userId = jQuery.urlParametro('userId');
+			
+			jQuery.getJSON('escritos/deleteHerNepes.php', {userId:userId})
+			.done(function(datos, estatusForDONE, xhrObjetoForDONE){  
+				//alert('key: ' + key + '\ndatos.isSet: ' + datos.isSet);
+				if(datos.nepesBorrados > 0)  {  }
+				else             {   }
+			})
+			.fail(function(xhrObjetoForFAIL, textoEstatus, elError){
+				var xhrObjetoForFAILString = JSON.stringify(  xhrObjetoForFAIL  );
+				var path = jQuery.encodeAndGetErrorPath(xhrObjetoForFAILString, textoEstatus, elError);
+				jQuery(window.location).attr('href', path);
+			});				
+		break;
+		case null:
+		default :
+		break;
+	}//acto switch
 
-		var look = jQuery.urlParametro('look');
-		switch(look) {
-			case 'busca':	
-				jQuery.lookYelScript('looks/busca.html', 'js/busca.js');
-			break;
-			case 'opciones':
-				jQuery.lookYelScript('looks/opciones.html', 'js/opciones.js');
-			break;
-			case 'viewNepe':
-				jQuery.lookYelScript('looks/viewNepe.html', 'js/nepe/viewNepe.js');
-			break;						
-			case 'login':
-				jQuery.lookYelScript('looks/login.html', 'js/dueno/login.js');
-				//jQuery(window.location).attr('href', window.location.pathname + '?look=home');		
-			break;
-			case 'home':
-				jQuery.lookYelScript('looks/home.html', 'js/dueno/home.js');
-				//error when solicita home sin estar logueado, se supone ni pueda
-			break;			
-			case 'creaNepe':
-				jQuery.lookYelScript('looks/creaNepe.html', 'js/nepe/creaNepe.js');		
-			break;
-			case 'updateNepe':
-				var key = 'dueno_id';
-				jQuery.getJSON('escritos/session/isSessionSet.php', {key:key})
-				.done(function(datos, estatusForDONE, xhrObjetoForDONE){  
-					//alert('key: ' + key + '\ndatos.isSet: ' + datos.isSet);
-					if(datos.isSet){
-						jQuery('#navLogin').hide();  jQuery('#navSignup').hide();
-						jQuery('#navHome').addClass("seleccionado");
-						jQuery('ul.navega').css('visibility','visible');
-						
-						key = 'own_nepes';
-						jQuery.getJSON('escritos/session/isSessionSet.php', {key:key})
-						.done(function(datos, estatusForDONE, xhrObjetoForDONE){  
-							//alert('key: ' + key + '\ndatos.isSet: ' + datos.isSet);
-							if(datos.isSet){  
-								key = 'own_nepes';
-								jQuery.getJSON('escritos/session/getSessionValue.php', {key:key})
-								.done(function(datos, estatusForDONE, xhrObjetoForDONE){  
-									//alert('key: ' + key + '\ndatos: ' + datos);
-									var nepeId = jQuery.urlParametro('nepeId');
-									if( jQuery.isNepeIdOnOwnNepesSession(datos, nepeId) ){
-										jQuery.getJSON('escritos/ojo/getNepe.php', {nepeId:nepeId} )
-										.done(function(datos, estatusForDONE, xhrObjetoForDONE){
-											jQuery.dameLook('looks/updateNepe.html');
-											jQuery(document).ajaxComplete(function(evento, xhrObjeto, settingsObjeto){
-												if(settingsObjeto.url === 'looks/updateNepe.html'){	
-													jQuery.hideThemSections();										
-													jQuery.populateUpdateNepeForm(datos);
-													jQuery('#footer').css('visibility','visible');
-													jQuery.handleUpdateNepeSubmit(nepeId);
-													//submit event listener and handler
-												}//if
-											});//ajaxComplete
-										})
-										.fail(function(xhrObjetoForFAIL, textoEstatus, elError){
-											var xhrObjetoForFAILString = JSON.stringify(  xhrObjetoForFAIL  );
-											var path = jQuery.encodeAndGetErrorPath(xhrObjetoForFAILString, textoEstatus, elError);
-											jQuery(window.location).attr('href', path); 
-										});
-									}else{
-										var datosJSONStrAsXHRTexto = 'Esto no es una respuesta del servidor.';
-										var textoEstatus = 'Error, usuario solicito editar nepe q no es de el.';
-										var elError = 'Error humano.';
-										
-										var path = jQuery.encodeAndGetErrorPath(datosJSONStrAsXHRTexto, textoEstatus, elError); // 
-										jQuery(window.location).attr('href', path);								
-									}
-								})
-								.fail(function(xhrObjetoForFAIL, textoEstatus, elError){
-									var xhrObjetoForFAILString = JSON.stringify(  xhrObjetoForFAIL  );
-									var path = jQuery.encodeAndGetErrorPath(xhrObjetoForFAILString, textoEstatus, elError);
-									jQuery(window.location).attr('href', path); 
-								});
-							}else{  
-								var datosJSONStrAsXHRTexto = 'Esto no es una respuesta del servidor.';
-								var textoEstatus = 'Error, usuario esta logueado pero sin own_nepes seteado. (for whatever reason)';
-								var elError = 'Error humano.';
-								
-								var path = jQuery.encodeAndGetErrorPath(datosJSONStrAsXHRTexto, textoEstatus, elError); // 
-								jQuery(window.location).attr('href', path);	
-							}
-						})
-						.fail(function(xhrObjetoForFAIL, textoEstatus, elError){
-							var xhrObjetoForFAILString = JSON.stringify(  xhrObjetoForFAIL  );
-							var path = jQuery.encodeAndGetErrorPath(xhrObjetoForFAILString, textoEstatus, elError);
-							jQuery(window.location).attr('href', path); 
-						});
-					}else{  
-						var datosJSONStrAsXHRTexto = 'Esto no es una respuesta del servidor.';
-						var textoEstatus = 'Error, usuario solicito updateNepe look, sin estar logueado.';
-						var elError = 'Error humano.';
+	var look = jQuery.urlParametro('look');
+	switch(look) {
+		case 'busca':	
+			jQuery.lookYelScript('looks/busca.html', 'js/busca.js');
+		break;
+		case 'opciones':
+			jQuery.lookYelScript('looks/opciones.html', 'js/opciones.js');
+		break;
+		case 'viewNepe':
+			jQuery.lookYelScript('looks/viewNepe.html', 'js/nepe/viewNepe.js');
+		break;						
+		case 'login':
+			jQuery.lookYelScript('looks/login.html', 'js/dueno/login.js');
+			//jQuery(window.location).attr('href', window.location.pathname + '?look=home');		
+		break;
+		case 'home':
+			jQuery.lookYelScript('looks/home.html', 'js/dueno/home.js');
+			//error when solicita home sin estar logueado, se supone ni pueda
+		break;			
+		case 'creaNepe':
+			jQuery.lookYelScript('looks/creaNepe.html', 'js/nepe/creaNepe.js');		
+		break;
+		case 'updateNepe':	
+			if(logueado){				
+					jQuery.getJSON('escritos/ojo/getNepe.php', {nepeId:updatingNepeIndex} )
+					.done(function(nepeDatos, estatusForDONE, xhrObjetoForDONE){
+						jQuery.lookYelScript('looks/updateNepe.html', 'js/nepe/updateNepe.js');
+						jQuery.populateUpdateNepeForm(nepeDatos);
+					})
+					.fail(function(xhrObjetoForFAIL, textoEstatus, elError){
+						var xhrObjetoForFAILString = JSON.stringify(  xhrObjetoForFAIL  );
+						var path = jQuery.encodeAndGetErrorPath(xhrObjetoForFAILString, textoEstatus, elError);
+						jQuery(window.location).attr('href', path); 
+					});
+			}else{  
+				var datosJSONStrAsXHRTexto = 'Esto no es una respuesta del servidor.';
+				var textoEstatus = 'Error, usuario solicito updateNepe look, sin estar logueado.';
+				var elError = 'Error humano.';
 
-						var path = jQuery.encodeAndGetErrorPath(datosJSONStrAsXHRTexto, textoEstatus, elError); // 
-						jQuery(window.location).attr('href', path);	
-					}
-				})
-				.fail(function(xhrObjetoForFAIL, textoEstatus, elError){
-					var xhrObjetoForFAILString = JSON.stringify(  xhrObjetoForFAIL  );
-					var path = jQuery.encodeAndGetErrorPath(xhrObjetoForFAILString, textoEstatus, elError);
-					jQuery(window.location).attr('href', path); 
-				});
-			break;			
-			case 'registro':
-				jQuery.lookYelScript('looks/registro.html', 'js/dueno/registro.js');
-				//jQuery(window.location).attr('href', window.location.pathname + '?look=home');
-			break;
-			case 'recentNepes':
-				jQuery.lookYelScript('looks/recentNepes.html', 'js/nepe/getRecentNepes.js');
-			break;			
-			case 'administrar':
-				jQuery.lookYelScript('looks/administrar.html', 'js/administrar.js');
-			break;		
-			case 'faq':
-				jQuery.lookYelScript('looks/faq.html', 'js/faq.js');
-			break;
-			case 'nada':
-				jQuery.lookYelScript('looks/nada.html', 'js/nada.js');
-			break;				
-			case 'error':
-				jQuery.lookYelScript('looks/error.html', 'js/error.js');
-			break;
+				var path = jQuery.encodeAndGetErrorPath(datosJSONStrAsXHRTexto, textoEstatus, elError); // 
+				jQuery(window.location).attr('href', path);	
+			}	
+		break;			
+		case 'registro':
+			jQuery.lookYelScript('looks/registro.html', 'js/dueno/registro.js');
+			//jQuery(window.location).attr('href', window.location.pathname + '?look=home');
+		break;
+		case 'recentNepes':
+			jQuery.lookYelScript('looks/recentNepes.html', 'js/nepe/getRecentNepes.js');
+		break;			
+		case 'administrar':
+			jQuery.lookYelScript('looks/administrar.html', 'js/administrar.js');
+		break;		
+		case 'faq':
+			jQuery.lookYelScript('looks/faq.html', 'js/faq.js');
+		break;
+		case 'nada':
+			jQuery.lookYelScript('looks/nada.html', 'js/nada.js');
+		break;				
+		case 'error':
+			jQuery.lookYelScript('looks/error.html', 'js/error.js');
+		break;
 
-			case '':
-				//alert('look vacio gmc');
-			break;
-			case null:
-				//alert('look null gmc');
-				jQuery(window.location).attr('href', window.location.pathname + '?look=recentNepes');
-			default:
-			break;
-		}//switch	
+		case '':
+			//alert('look vacio gmc');
+		break;
+		case null:
+			//alert('look null gmc');
+			jQuery(window.location).attr('href', window.location.pathname + '?look=recentNepes');
+		default:
+		break;
+	}//look switch	
 
 }); // ready function and statement
--- js/navega.js
+++ js/navega.js
@@ -1,5 +1,16 @@
 jQuery(document).ready(function(){
 
+	alert('logueado en navega js: ' + logueado);
+	if(logueado){ 
+		jQuery('#navLogout').css('visibility','visible').show();
+		jQuery('#navHome').css('visibility','visible').show()  ;
+		alert('navega js:\nshowing logout and home nav buttons for: \nlogueado=' + logueado);
+	}else{
+		jQuery('#navLogin').css('visibility','visible').show();     
+		jQuery('#navSignup').css('visibility','visible').show();
+		alert('navega js:\nshowing login and sign up nav buttons for:: \nlogueado=' + logueado);
+	}
+	
 	let look = jQuery.urlParametro('look');
 	switch(look){
 		case 'busca':
@@ -10,9 +21,8 @@ jQuery(document).ready(function(){
 		break;
 		case 'home':
 		case 'administrar':
-			jQuery('#navHome').addClass("seleccionado");
-		break;
 		case 'creaNepe':
+		case 'updateNepe':
 			jQuery('#navHome').addClass("seleccionado");
 		break;
 		case 'registro':
@@ -31,29 +41,7 @@ jQuery(document).ready(function(){
 		case 'viewNepe':
 		default:
 		break;
-	}
+	}//switch
 
-	//navigation botton that depend on logged condition
-	jQuery.getJSON('escritos/session/isSessionSet.php', {key:'dueno_id'})
-	.done(function(logueadoRespuesta, estatusForDONE, xhrObjetoForDONE){
-		//alert('for dueno_id ... logueadoRespuesta.isSet: ' + logueadoRespuesta.isSet);
-		let logueado = logueadoRespuesta.isSet;
-		console.log('logueado local in DONE:' + logueado);
-		if(logueado){ 
-			jQuery('#navLogout').css('visibility','visible').show();
-			jQuery('#navHome').css('visibility','visible').show()  ;
-			//alert('is sesion set: logueado');
-		 }else{
-			jQuery('#navLogin').css('visibility','visible').show();     
-			jQuery('#navSignup').css('visibility','visible').show();
-			//alert('is session set: NO logueado');
-		}
-	})
-	.fail(function(xhrObjetoForFAIL, textoEstatus, elError){
-		var xhrObjetoForFAILString = JSON.stringify(  xhrObjetoForFAIL  );
-		var path = jQuery.encodeAndGetErrorPath(xhrObjetoForFAILString, textoEstatus, elError);
-		jQuery(window.location).attr('href', path); 
-	});
-
-});
+});//ready function and statement
 
