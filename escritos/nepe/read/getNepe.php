14bf8eaf00b802b09dc8f3a40ff1c6f33fcafdf4
victor j. perez <vijapefi@gmail.com>
2024-02-29 17:51:18 -0400

    read get nepe for main nepe

--- escritos/nepe/read/getNepe.php
+++ escritos/nepe/read/getNepe.php
@@ -1,4 +1,7 @@
 <?php
+//lo que hay q sacar de la basedatos logueado y con sesson definida para hacer update Nepe,
+//es la mismo info q hace falta sacar para mostrar el profile del nepe publicamente
+//en look=view nepe.  Pero el url trae cosas diferentes en el get
 session_start();
 if( isset($_SESSION['dueno_id']) && isset($_SESSION['own_nepes_with_ids']) && isset($_GET['nepe_index'])){
 	$own_nepes_with_ids = $_SESSION['own_nepes_with_ids'];
@@ -26,50 +29,21 @@ if($recurso){
 	$nepeDato = array();
 	if($fila = pg_fetch_row($recurso)){
 		//Read data already in json format, and decode it into PHP variables with correct datatype
-		$nepeDato['nepeId'] = json_decode($fila[0]);
-		$nepeDato['nombre'] = $fila[1];
-		$nepeDato['revisado'] = $fila[2];
-		$nepeDato['que'] = json_decode($fila[3]);
-		$nepeDato['donde'] = json_decode($fila[4]);
-		$nepeDato['cuando'] = json_decode($fila[5]);
-		$nepeDato['atucasa'] = json_decode($fila[6]);
-
-		$nepeDato['videoUrl'] = $fila[7];	
-
-		$nepeDato['quienSocialHandle'] = json_decode($fila[8]);
-		$nepeDato['quienFotoSrc'] = json_decode($fila[9]);
-
-		$nepeDato['videoCode'] = videoCode( $fila[7] );
+		$nepeDato['nepeId']    = json_decode($fila[0]);
+		$nepeDato['creado']    = $fila[1];
+		$nepeDato['nombre']    = $fila[2];
+		$nepeDato['cuando']    = json_decode($fila[3]);
+		$nepeDato['sucasa']    = json_decode($fila[4]);
+		$nepeDato['desdecasa'] = json_decode($fila[5]);
 		pg_close($cnx); 
 		//Send data from server in json format
 		echo json_encode($nepeDato);
 	}else{
-		pg_close($cnx); //maybe not needed but doesn't hurt
+		pg_close($cnx);
 		throw new Exception('Con RECURSO, pero sin $fila, (nepe id no existe) en :'  .  __FILE__  .  '.');	
 	}
 }else{
-	pg_close($cnx); //maybe not needed but doesn't hurt
+	pg_close($cnx);
 	throw new Exception('Mal query.  Sin RECURSO para preparadoQueryGetNepe en :'  .  __FILE__  .  '.');
-	//echo "<li>Error, pg_query, no produjo un recurso ... en .....</li>";
 }
-
-
-//lo que hay q sacar de la basedatos para q al loguarte hacer updateNepe
-//es la mismo info q hace falta sacar para mostrar el profile del nepe publicamente
-//en look=profile 
-//Solo que en update nepe tambien se anaden los datos del admin.
-
-
-
-function videoCode($url) { // mide al menos 11 y tiene youtu; youtu123456 aun siendo invalido,devuelve true
-	if(strlen($url) === 0){
-		return 0;  
-	}elseif (  ! (strpos($url, 'youtu') === false)  ){	// 'youtu' is somewhere
-		return 1;
-	}else {
-		return 2;
-	}
-} 
- 
-
 ?>
