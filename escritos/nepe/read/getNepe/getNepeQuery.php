338a4a330e4affc07bb06fc3c4bdcbc2f77365f0
victor j. perez <vijapefi@gmail.com>
2024-02-29 17:54:01 -0400

    read get nepe query for main section

--- escritos/nepe/read/getNepe/getNepeQuery.php
+++ escritos/nepe/read/getNepe/getNepeQuery.php
@@ -1,27 +1,18 @@
 <?php
-//When you put bool values, arrays, or integers like the micro empre id, into a json format it allows
-//the later use of functions like json_decode().  This makes possible
-//the preservation of the datatypes, as stored in  postgresql.
+//When you put bool values, arrays, or integers like nepe id, into a json format it allows
+//the later use of functions like json_decode(), making it possible
+//to preservate datatypes, as stored in  postgresql.
 //When jQuery.getJSON receives data it will build a javascript objects
 //with the correct datatypes only if you preserve those datatypes,
 //otherwise it simply receives text, and you get hard to debug,
 //wrong results.
-
-
-
-//query returns arrays, bool values ... everything in JSON format (read data from postgre, as json)
 $queryGetNepe = "SELECT
 	to_json(id),
+	to_char(creado, 'MM/DD/YYYY'),
 	nombre,
-	to_char(revisado, 'MM/DD/YYYY'),
-	array_to_json(que),
-	array_to_json(donde),	
-	cuando, 
-	to_json(a_tu_casa), 
-
-	media_video_url, 
-	media_social_handle, 
-	array_to_json(media_foto_url) 
+	cuando,
+	to_json(su_casa),
+	to_json(desde_casa)
 FROM nepe 
 WHERE id = $1";
 pg_prepare($cnx, "preparadoQueryGetNepe", $queryGetNepe);
