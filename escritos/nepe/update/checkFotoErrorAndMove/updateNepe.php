c89b63974a8af98ab88e1d28f82baf763c238409
victor j. perez <vijapefi@gmail.com>
2024-03-04 18:33:21 -0400

    update nepe files checking image upload error

--- escritos/nepe/update/checkFotoErrorAndMove/updateNepe.php
+++ escritos/nepe/update/checkFotoErrorAndMove/updateNepe.php
@@ -2,11 +2,10 @@
 require_once 'updateNepeQuery.php';
 if($recurso){
 	$respuesta = json_decode('{"actualizado":true, "feedback":"Nepe actualizado, incluyendo fotos.", "nepeId":' . $nepe_id . '}');
-	pg_close($cnx); //maybe not needed but doesn't hurt
+	pg_close($cnx);
 	echo json_encode ($respuesta);
 }else{
-	pg_close($cnx); //maybe not needed but doesn't hurt
-	//echo $query;
-	throw new Exception('Mal query.  Sin RECURSO, para query updateNepeQuery en: ' . __FILE__);
+	pg_close($cnx);
+	throw new Exception('Mal query.  Sin RECURSO, para preparedQueryUpdateNepe en: ' . __FILE__);
 }
 ?>
