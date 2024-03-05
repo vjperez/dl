8b97f00e827f631c4fe95b6963ecb1fe56aac3e8
victor j. perez <vijapefi@gmail.com>
2024-03-05 11:16:23 -0400

    remove file

--- utils/setGitHubToken.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-git remote remove dlAlla
-git remote add dlAlla https://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@github.com/vjperez/dl
-git remote -v
-git push --set-upstream dlAlla master
