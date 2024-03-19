84a0838c18c3d447272b1b8fd1bb237567096d45
victor j. perez <vijapefi@gmail.com>
2024-03-19 14:12:53 -0400

    adding func call to prepare queries for diferent tables for que-donde nepe update

--- escritos/nepe/update/que-donde/update.php
+++ escritos/nepe/update/que-donde/update.php
@@ -5,14 +5,20 @@ if( isset($_SESSION['dueno_id']) && isset($_SESSION['own_nepes_with_ids']) ){
 	$nepe_id = $own_nepes_with_ids[$index]['nepeId'];
 	
 	
-	require_once 'updateQueries.php';
-	$toBeIdsArr = getOrCreateToBeIds('que', $queToBeFrasesArr, $cnx);
-	$areIdsArr  = getCurrentIds('que',   $nepe_id, $cnx);
-	makeAndRemoveLinks('que',   $nepe_id, $cnx);
+	require_once 'update/que-donde/updateQueries.php';
+	prepareToBeIdsQueries('Que', $cnx);
+	$toBeIdsArr = getOrCreateToBeIds('Que', $queToBeFrasesArr, $cnx);
+	prepareCurrentIdsQueries('Que', $cnx);
+	$areIdsArr  = getCurrentIds('Que',   $nepe_id, $cnx);
+	prepareMakeRemoveQueries('Que', $cnx);
+	makeAndRemoveLinks('Que', $nepe_id, $areIdsArr, $toBeIdsArr, $cnx);
 	
-	$toBeIdsArr = getOrCreateToBeIds('donde', $dondeToBeFrasesArr, $cnx);
-	$areIdsArr  = getCurrentIds('donde',   $nepe_id, $cnx);
-	makeAndRemoveLinks('donde',   $nepe_id, $cnx);
+	prepareToBeIdsQueries('Donde', $cnx);
+	$toBeIdsArr = getOrCreateToBeIds('Donde', $dondeToBeFrasesArr, $cnx);
+	prepareCurrentIdsQueries('Donde', $cnx);
+	$areIdsArr  = getCurrentIds('Donde',   $nepe_id, $cnx);
+	prepareMakeRemoveQueries('Donde', $cnx);
+	makeAndRemoveLinks('Donde', $nepe_id, $areIdsArr, $toBeIdsArr, $cnx);
 	
 	$respuesta = json_decode('{"nepeQue-DondeUpdated":true}');
 	pg_close($cnx);
