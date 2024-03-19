63c029f44dd7f0f23bcda3c7778fe1f76bf4fea7
victor j. perez <vijapefi@gmail.com>
2024-03-19 14:08:22 -0400

    removing creado from core nepe update

--- escritos/nepe/update/core/updateQueries.php
+++ escritos/nepe/update/core/updateQueries.php
@@ -1,7 +1,7 @@
 <?php
 //updates nepe core parts
 $queryUpdateNepe = "UPDATE nepe SET
- 	nombre=$2, cuando=$3, su_casa=$4, desde_casa=$5, creado=NOW()::date, revisado=NOW()::date
+ 	nombre=$2, cuando=$3, su_casa=$4, desde_casa=$5, revisado=NOW()::date
 	WHERE id=$1";
 pg_prepare($cnx, "preparadoQueryUpdateNepe", $queryUpdateNepe);
 
