6317976597501d23da2feb3c6b42bffe4ab77213
victor j. perez <vijapefi@gmail.com>
2024-03-20 16:57:19 -0400

    link on get recent nepe

--- js/nepe/getRecentNepes.js
+++ js/nepe/getRecentNepes.js
@@ -2,17 +2,16 @@ jQuery.getJSON('escritos/nepe/read/getRecentNepes.php')
 .done(function(datos, estatusForDONE, xhrObjetoForDONE){
 	var tableRows = '';
 	jQuery.each(datos, function(index){
+			let link = '"portada.html?look=viewNepe&nepeId=' + datos[index].nepeId  + '"';
 			tableRows += '<tr class="texto"><td>' 
-			+ '<a class="" href="portada.html?look=profile'
-			+  '&nepeId=' + datos[index].nepeId  + '">' 
-			+ datos[index].nepeNombre + '   <span class="diasOld">[' +  datos[index].dias  +  ' dias]</span>'
+			+ '<a href=' + link + '>' 
+			+ datos[index].nepeNombre + '<span class="diasOld">[' +  datos[index].dias  +  ' dias]</span>'
 			+'</a>' 
 			+ '</td></tr>'; 
 
 			tableRows +=  '<tr class="foto"><td>' 
-			+ '<a class="" href="portada.html?look=profile'
-			+  '&nepeId=' + datos[index].nepeId  + '">' 
-			+ '<img class="" src="imagenes/nepe/subidas/' + datos[index].nepeFotoName + '">'
+			+ '<a href=' + link + '>' 
+			+ '<img src="imagenes/nepe/subidas/' + datos[index].nepeFotoName + '">'
 			+ '</a>' 
 			+ '</td></tr>'; 
 			tableRows += '<tr><td></td></tr><tr><td></td></tr>';
