fbf2680e4cd6ba3bfc62cac0ee7df8f3da39db6f
victor j. perez <vijapefi@gmail.com>
2024-03-21 14:03:38 -0400

    removing query join

--- escritos/dueno/read/socialsQuery.php
+++ escritos/dueno/read/socialsQuery.php
@@ -2,9 +2,8 @@
 //joins dueno and social to get tipo and hangle using id
 $querySocials = "SELECT
     tipo, handle
-FROM dueno 
-INNER JOIN social  ON  social.dueno_id = dueno.id
-WHERE dueno.id = $1";
+FROM social
+WHERE dueno_id = $1";
 
 pg_prepare($cnx, "preparadoQuerySocials", $querySocials);
 ?>
