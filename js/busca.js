017ffe74bbba63fd408e96e1dad521a96cb8ad77
victor j. perez <vijapefi@gmail.com>
2024-03-23 00:42:30 -0400

    including blank space on regexp for busca

--- js/busca.js
+++ js/busca.js
@@ -1,6 +1,6 @@
 jQuery('form').submit(function(evento){
     evento.preventDefault(); //not making a submit (GET request) here. Lets do it at look=opciones
-    var regexp = new RegExp(/[^a-z0-9ñüàáèéìíòóùú@._+-]/gi);	//see 'negated or complemented character class'; basically what characters are not letters, numbers or these symbols
+    var regexp = new RegExp(/[^a-z0-9\sñüàáèéìíòóùú@._+-]/gi);	//see 'negated or complemented character class'; basically what characters are not letters, numbers or these symbols
     var que = jQuery('#queId').val();
     que = jQuery.cleanStr(que, regexp); // clean function returns cleaned str
     var donde = jQuery('#dondeId').val();
