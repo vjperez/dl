91693728c4e194bd1ce5367ef58be01dc979539e
victor j. perez <vijapefi@gmail.com>
2024-03-24 23:14:36 -0400

    removing some jquery each, now reading array of asoc arrays on opciones js

--- js/opciones.js
+++ js/opciones.js
@@ -1,28 +1,32 @@
 let que   = decodeURIComponent( jQuery.urlParametro('que') );      
 let donde = decodeURIComponent( jQuery.urlParametro('donde') ); 
-alert('[' + que + '][' + donde + ']');
+//alert('[' + que + '][' + donde + ']');
 jQuery.getJSON('escritos/nepe/read/getOpciones.php', {que:que, donde:donde} )
 .done(function(datos, estatusForDONE, xhrObjetoForDONE){
-	if(datos.cuantasOpciones > 0){
+	if(datos.length > 0){
 		//alert('datos: automatically parsed to object object por getJSON = ' + datos + '\nxhrObjetoForDONE.status = ' + xhrObjetoForDONE.status + '\nxhrObjetoForDONE.statustext = ' + xhrObjetoForDONE.statusText + '\nestatusForDONE = ' + estatusForDONE );
 		var htmlForMain = '';
-		jQuery.each(datos.opciones, function(buscaMode, trios){
-				htmlForMain += '<div id="opcionesdiv" class="opcionesfotos ">';
-				if(buscaMode.indexOf("buscaBoth") > -1){
-					htmlForMain += '<h2 class="notHidable">'  + que + ' + ' + donde + '</h2>';
-				}else if (buscaMode.indexOf("buscaQue") > -1){
-					htmlForMain += '<h2 class="notHidable">'  + que + '</h2>';
-				}else if (buscaMode.indexOf("buscaDonde") > -1){
-					htmlForMain += '<h2 class="notHidable">'  + donde + '</h2>';
-				}
-				jQuery.each(trios, function(index, pares){
-					jQuery.each(pares, function(nepeId, fotoSrc){
-						htmlForMain += '<a href="portada.html?look=viewNepe&nepeId=' + nepeId + '">';
-						htmlForMain += '<img class="" src="imagenes/nepe/subidas/' + fotoSrc + '">'; 
-						htmlForMain += '</a>';
-					});
-				}); // each in trios
-				htmlForMain += '</div>'; // <div class="ver-borde opcionesfotos">
+		jQuery.each(datos, function(index){
+			alert( datos[index].nepeId );
+			htmlForMain += '<div id="opcionesdiv" class="opcionesfotos ">';
+
+
+			buscaMode = datos[index].buscaMode;
+			if(buscaMode.indexOf("buscaBoth") > -1){
+				htmlForMain += '<h2 class="notHidable">'  + que + ' + ' + donde + '</h2>';
+			}else if (buscaMode.indexOf("buscaQue") > -1){
+				htmlForMain += '<h2 class="notHidable">'  + que + '</h2>';
+			}else if (buscaMode.indexOf("buscaDonde") > -1){
+				htmlForMain += '<h2 class="notHidable">'  + donde + '</h2>';
+			}
+			nepeId =  datos[index].nepeId;
+			fotoUrl = datos[index].fotoUrl;
+			htmlForMain += '<a href="portada.html?look=viewNepe&nepeId=' + nepeId + '">';
+			htmlForMain += '<img class="" src="imagenes/nepe/subidas/' + fotoUrl + '">'; 
+			htmlForMain += '</a>';
+			
+			
+			htmlForMain += '</div>'; // <div class="ver-borde opcionesfotos">
 		}); // each in datos
 		jQuery('#main').html(htmlForMain);
 	}else{
