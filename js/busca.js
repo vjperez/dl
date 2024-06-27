e90690f5fafa2cdb2b2f015b0fa6e4c14ff395d1
victor j. perez <vijapefi@gmail.com>
2024-06-27 14:26:38 -0400

    busca plain js

--- js/busca.js
+++ js/busca.js
@@ -1,16 +1,23 @@
-jQuery('form').submit(function(evento){
-    evento.preventDefault(); //not making a submit (GET request) here. Lets do it at look=opciones
-    var regexp = new RegExp(/[^a-z0-9\sñüàáèéìíòóùú@._+-]/gi);	//see 'negated or complemented character class'; basically what characters are not letters, numbers or these symbols
-    var que = jQuery('#queId').val();
-    que = jQuery.cleanStr(que, regexp); // clean function returns cleaned str
-    var donde = jQuery('#dondeId').val();
-    donde = jQuery.cleanStr(donde, regexp); // clean function returns cleaned str
-    //alert('que=(' + que  + ')\ndonde=(' +  donde + ')');
-    if(que.length > 0 || donde.length > 0){//i'm looking for a non empty cleaned str
-        jQuery(window.location).attr('href', window.location.pathname + '?look=opciones&que=' + encodeURIComponent(que) 
-        + '&donde=' + encodeURIComponent(donde)  );
-    }else{
-        let feedbackStr = 'Buscas algo?';
-        jQuery.feedback('form#queDondeForm  h3.feedback', feedbackStr, 'feedbackwarn', 'downdelayup');
-    }
-});    
+const forma = document.querySelector('form#queDondeForm');
+forma.addEventListener('submit', handleSubmit);
+
+function handleSubmit(evento) {
+  evento.preventDefault(); //not making a submit (GET request) here. Lets do it at look=opciones
+  let regexp = new RegExp(/[^a-z0-9\sñüàáèéìíòóùú@._+-]/gi);
+  //see 'negated or complemented character class';
+  //basically what characters are not letters, numbers or these symbols
+
+  let que = document.querySelector('#queId').value;
+  que = cleanStr(que, regexp);
+  let donde = document.querySelector('#dondeId').value;
+  donde = cleanStr(donde, regexp);
+  //alert('que=(' + que  + ')\ndonde=(' +  donde + ')');
+  if (que.length > 0 || donde.length > 0) {
+    //i'm looking for a non empty cleaned str
+    window.location.href = 
+      window.location.pathname + '?look=opciones&que=' + encodeURIComponent(que) + '&donde=' + encodeURIComponent(donde);
+  } else {
+    let feedbackStr = 'Buscas algo?';
+    feedback('form#queDondeForm  h3.feedback', feedbackStr, 'feedbackwarn', 'downdelayup');
+  }
+}
