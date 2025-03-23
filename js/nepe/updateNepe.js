f50994aaea5b46083649d67efeaa25c03aa49630
victor j. perez <vijapefi@gmail.com>
2025-03-23 13:25:30 -0400

    removing spaces and minimize str on update nepe

--- js/nepe/updateNepe.js
+++ js/nepe/updateNepe.js
@@ -49,7 +49,7 @@ populateUpdateNepeForm = function(datos){
 				queEl.value = losQue[indice]; 
 			}
 			//the html is prepared for a max of 10 'que'
-			//en submit entran al arreglo, luego de pasar por cleanStr() y  isNotVacioStr()		  
+			//en submit entran al arreglo, luego de pasar por minimizeStr() y  isNotVacioStr()		  
 		});
 	}
 	
@@ -63,7 +63,7 @@ populateUpdateNepeForm = function(datos){
 				dondeEl.value = losDonde[indice];
 			}
 			//the html is prepared for a max of 5 'donde'
-			//en submit entran al arreglo, luego de pasar por cleanStr() y  isNotVacioStr()
+			//en submit entran al arreglo, luego de pasar por minimizeStr() y  isNotVacioStr()
 		});
 	}
 }//populate function 
@@ -100,185 +100,150 @@ hideThemSections();
 
 
 //task 2
-//build formdata with cleanStr() and isVacioStr() returned values ... and make post
 let forma = document.querySelector('form#nepeForm');
+let formData = new FormData(forma);
 forma.addEventListener('submit',
 function(evento){
 	evento.preventDefault();
-	//fotoArr();
 
-	//setTimeout(function(){ fotoArr(); }, 0);
-	//setTimeout(function(){ buildFormData(); }, 200);
-	//setTimeout(function(){ postea(); }, 400);	
-
-  fotoArr( buildFormData( postea ) );
+  buildFormData( postea );
 });  //submit
 
 
-	function fotoArr( callbackBuildFormFunction ){
-		//foto
-		//let filesOnFotoArr = formData.getAll("fotoArr[]");
-		//la linea de abajo para filesOnFotoArr, works tambien
-		let selector = 'fieldset#fotoFieldset   input[name^=fotoArr]';
-		let filesOnFotoArr = document.querySelector(selector).files;
-		console.log(filesOnFotoArr);
-		if( filesOnFotoArr.length === 0){
-			formData.delete("fotoArr[]");     //tested with isset on php
-			formData.delete("MAX_FILE_SIZE");
-		}else if( filesOnFotoArr.length > 0 ){ 
-			ponScript('js/nepe/resizeImage.js');
-			
-			formData.delete("fotoArr[]"); 
-			for(let index = 0; index < filesOnFotoArr.length; index++){
-				let unFile = filesOnFotoArr[index];
-				if( filesOnFotoArr[index].type.toLowerCase().startsWith("image") ){
-					console.log("calling resizeImage(), file:" + index);
-					//resizeImage(index, unFile, formData);
-					setTimeout(function(){ resizeImage(index, unFile, formData); }, 100);
-				}else{ 
-					console.log("skipping file:" + index);
-					continue; 
-				}
-			}//for
+    function buildFormData( callbackPostFunction ){
+      let regexpNQD =    new RegExp(/[^a-z0-9ñäàáëèéïìíöòóüùú@._+-\s]/gi);	//	allowing letters, numbers plus los de login   @ . _ + -
+      let regexpCuando = new RegExp(/[^a-z0-9ñäàáëèéïìíöòóüùú:,@._+-\s]/gi);	//	allowing letters, numbers plus los de login   @ . _ + -	  y   : ,	
+      //let regexpURL =    new RegExp(/[^a-z0-9.:/=?&@._+-\s]/gi);	//	allowing letters, numbers and simbols needed for a url .:/=?& plus los de login   @ . _  + -
 
-		}//else if
-
-    	callbackBuildFormFunction();
-	}//fotoArr function
-
-	let formData = new FormData(forma);
-	function buildFormData( callbackPostFunction ){
-    let regexpNQD =    new RegExp(/[^a-z0-9ñäàáëèéïìíöòóüùú@._+-\s]/gi);	//	allowing letters, numbers plus los de login   @ . _ + -
-	let regexpCuando = new RegExp(/[^a-z0-9ñäàáëèéïìíöòóüùú:,@._+-\s]/gi);	//	allowing letters, numbers plus los de login   @ . _ + -	  y   : ,	
-    let regexpURL =    new RegExp(/[^a-z0-9.:/=?&@._+-\s]/gi);	//	allowing letters, numbers and simbols needed for a url .:/=?& plus los de login   @ . _  + -
-
-    // a) build and edit formdata
-
-    // core nepe //
-    
-		//nombre
-		let nombre = cleanStr( document.querySelector('fieldset#nombreFieldset input[name=nombre]').value, regexpNQD );
-		if(isVacioStr(nombre)){
-			formData.delete("nombre");      formData.append('nombre', 'no-name-provided');
-		}else{
-			formData.delete("nombre"); 			formData.append('nombre', nombre);
-		}
-		
-		//cuando is a JS object, ... converted to string in JSON format
-		let cuando = {  
-			lun:cleanStr( document.querySelector('fieldset#cuandoFieldset input[name=dia1]').value, regexpCuando ), 
-			mar:cleanStr( document.querySelector('fieldset#cuandoFieldset input[name=dia2]').value, regexpCuando ),
-			mie:cleanStr( document.querySelector('fieldset#cuandoFieldset input[name=dia3]').value, regexpCuando ),
-			jue:cleanStr( document.querySelector('fieldset#cuandoFieldset input[name=dia4]').value, regexpCuando ),
-			vie:cleanStr( document.querySelector('fieldset#cuandoFieldset input[name=dia5]').value, regexpCuando ),
-			sab:cleanStr( document.querySelector('fieldset#cuandoFieldset input[name=dia6]').value, regexpCuando ),
-			dom:cleanStr( document.querySelector('fieldset#cuandoFieldset input[name=dia7]').value, regexpCuando )
-		};
-		formData.delete("dia1"); //sending all dias in a JSON string, so delete individual values from formData
-		formData.delete("dia2"); 
-		formData.delete("dia3"); 
-		formData.delete("dia4");
-		formData.delete("dia5"); 
-		formData.delete("dia6"); 
-		formData.delete("dia7"); 
-		cuando = JSON.stringify(cuando);
-		formData.append('cuando', cuando);
-
-		//desdeCasa - suCasa
-		let suCasa = document.querySelector('input[name="suCasa"]:checked').value;
-		formData.append('suCasa', suCasa);
-		let desdeCasa = document.querySelector('input[name="desdeCasa"]:checked').value;
-		formData.append('desdeCasa', desdeCasa);
-    //  end of core nepe //
-
-    
-    //  beyond core nepe //
-		//video
-    let videoUrl = cleanStr( document.querySelector('fieldset#videoFieldset input[name=videoUrl]').value, regexpURL );
-    
-    if(isVacioStr(videoUrl)){
-			formData.delete("videoUrl"); 		//tested with isset on php
-      		//append nothing, for isset on php to work    formData.append('videoUrl', 'no-video-provided');
-		}else{
-			formData.delete("videoUrl"); 		formData.append('videoUrl', videoUrl);
-		}
-		
-		
-		//que is a JS array, ... converted to string in JSON format
-		let que = new Array();
-		document.querySelectorAll('fieldset#queFieldset input[name^=que]')
-		.forEach(
-		function(inputEl, indice){
-			let cleanedQue = cleanStr(inputEl.value, regexpNQD );
-			if(isNotVacioStr(cleanedQue)) { que[indice] = cleanedQue; }
-			formData.delete( inputEl.getAttribute("name") ); //sending all ques in JSON string so delete them individually from formData
-		});
-		que = JSON.stringify(que); 
-		formData.append('losQue', que);
-		
-		
-		//donde is a JS array, ... converted to string in JSON format
-		let donde = new Array();
-		document.querySelectorAll('fieldset#dondeFieldset input[name^=donde]')
-		.forEach(
-		function(inputEl, indice){
-			let cleanedDonde = cleanStr(inputEl.value, regexpNQD );
-			if(isNotVacioStr(cleanedDonde)) { donde[indice] = cleanedDonde; }
-			formData.delete( inputEl.getAttribute("name") ); //sending all dondes in JSON string so delete them individually from formData
-		});
-		donde = JSON.stringify(donde);
-		formData.append('losDonde', donde);
-		// end of beyond core nepe //
-
-
-		formData.append('nepe_index', index );
-		
-		console.log("la forma...");
-		for (const pareja of formData.entries()) {
-			console.log('llave: ' + pareja[0] + '   valor: ' + pareja[1]);
-		}
-		//formdata built
-
-    //callbackPostFunction();
-	}//buildFormData function
-
-
-	function postea(){ 
-	  //alert('@ postea despues de delay...');
-	  const opciones = { body:formData, method:'post' };
-	  fetch('escritos/nepe/update.php', opciones )
-	  .then(
-	  function(respuesta){
-      console.log(' fetch, then 1');
-      console.log(respuesta);
-      return respuesta.text();  
-	  })
-	  .then(
-	  function(datos){
-      console.log(' fetch, then 2: ');
-      console.log( datos );
-      /////////////////////////try catch////////////////////////
-      let datosJSOBJ;
-      try{
-        datosJSOBJ = JSON.parse( datos );
-      }
-      catch( err ){
-        throw new Error( err + '<br><br>' + datos ); 
-      }
-      //////////////////////////////////////////////////////////
-      if(datosJSOBJ.nepeCoreUpdated && datosJSOBJ.queDondeUpdated && (datosJSOBJ.videoInserted || datosJSOBJ.videoUpdated  || datosJSOBJ.videoRemoved || datosJSOBJ.videoNothingDone )){
-        window.location.href = window.location.pathname + '?look=viewNepe&nepeId=' + datosJSOBJ.nepeId;
+      // a) build and edit formdata  // core nepe //
+      
+      //nombre
+      let nombre = removeExtraSpacesAndChars( document.querySelector('fieldset#nombreFieldset input[name=nombre]').value, regexpNQD );
+      if(isVacioStr(nombre)){
+        formData.delete("nombre");      formData.append('nombre', 'no-name-provided');
       }else{
-        //falta algo ?
-        //feedback('form#updateNepeForm h2', datosJSOBJ.feedback);
-      } 
-	  })
-	  .catch(
-	  function(error){
-      const href = encodeAndGetErrorPath(error);
-      window.location.href = href;
-	  });
-	}//postea function
+        formData.delete("nombre"); 			formData.append('nombre', nombre);
+      }
+      
+      //cuando is a JS object, ... converted to string in JSON format
+      let cuando = {  
+        lun:removeExtraSpacesAndChars( document.querySelector('fieldset#cuandoFieldset input[name=dia1]').value, regexpCuando ), 
+        mar:removeExtraSpacesAndChars( document.querySelector('fieldset#cuandoFieldset input[name=dia2]').value, regexpCuando ),
+        mie:removeExtraSpacesAndChars( document.querySelector('fieldset#cuandoFieldset input[name=dia3]').value, regexpCuando ),
+        jue:removeExtraSpacesAndChars( document.querySelector('fieldset#cuandoFieldset input[name=dia4]').value, regexpCuando ),
+        vie:removeExtraSpacesAndChars( document.querySelector('fieldset#cuandoFieldset input[name=dia5]').value, regexpCuando ),
+        sab:removeExtraSpacesAndChars( document.querySelector('fieldset#cuandoFieldset input[name=dia6]').value, regexpCuando ),
+        dom:removeExtraSpacesAndChars( document.querySelector('fieldset#cuandoFieldset input[name=dia7]').value, regexpCuando )
+      };
+      formData.delete("dia1"); //sending all dias in a JSON string, so delete individual values from formData
+      formData.delete("dia2"); 
+      formData.delete("dia3"); 
+      formData.delete("dia4");
+      formData.delete("dia5"); 
+      formData.delete("dia6"); 
+      formData.delete("dia7"); 
+      cuando = JSON.stringify(cuando);
+      formData.append('cuando', cuando);
+
+      //desdeCasa - suCasa
+      formData.delete("suCasa"); 
+      formData.delete("desdeCasa");
+      let suCasa = document.querySelector('input[name="suCasa"]:checked').value;
+      formData.append('suCasa', suCasa);
+      let desdeCasa = document.querySelector('input[name="desdeCasa"]:checked').value;
+      formData.append('desdeCasa', desdeCasa);
+      //  end of core nepe //
+
+      
+      //  beyond core nepe //
+      //video
+      //let videoUrl = removeExtraSpacesAndChars( document.querySelector('fieldset#videoFieldset input[name=videoUrl]').value, regexpURL );
+      let videoUrl = document.querySelector('fieldset#videoFieldset input[name=videoUrl]').value;
+
+      if(isVacioStr(videoUrl)){
+        formData.delete("videoUrl"); 		//tested with isset on php
+            //append nothing, for isset on php to work    formData.append('videoUrl', 'no-video-provided');
+      }else{
+        formData.delete("videoUrl"); 		formData.append('videoUrl', videoUrl);
+      }
+      
+      
+      //que is a JS array, ... converted to string in JSON format
+      let que = new Array();
+      document.querySelectorAll('fieldset#queFieldset input[name^=que]')
+      .forEach(
+      function(inputEl, indice){
+        let cleanedQue = minimizeStr(inputEl.value, regexpNQD );
+        if(isNotVacioStr(cleanedQue)) { que[indice] = cleanedQue; }
+        formData.delete( inputEl.getAttribute("name") ); //sending all ques in JSON string so delete them individually from formData
+      });
+      que = JSON.stringify(que); 
+      formData.append('losQue', que);
+      
+      
+      //donde is a JS array, ... converted to string in JSON format
+      let donde = new Array();
+      document.querySelectorAll('fieldset#dondeFieldset input[name^=donde]')
+      .forEach(
+      function(inputEl, indice){
+        let cleanedDonde = minimizeStr(inputEl.value, regexpNQD );
+        if(isNotVacioStr(cleanedDonde)) { donde[indice] = cleanedDonde; }
+        formData.delete( inputEl.getAttribute("name") ); //sending all dondes in JSON string so delete them individually from formData
+      });
+      donde = JSON.stringify(donde);
+      formData.append('losDonde', donde);
+      // end of beyond core nepe //
+
+
+      formData.append('nepe_index', index );
+      //formdata built
+    
+
+    
+      console.log("la forma...");
+      for (const pareja of formData.entries()) {
+        console.log('llave: ' + pareja[0] + '   valor: ' + pareja[1]);
+      }
+      callbackPostFunction();
+    }//buildFormData function
+
+
+    function postea(){ 
+      //alert('@ postea despues de delay...');
+      const opciones = { body:formData, method:'post' };
+      fetch('escritos/nepe/update.php', opciones )
+      .then(
+      function(respuesta){
+        console.log(' fetch, then 1');
+        console.log(respuesta);
+        return respuesta.text();  
+      })
+      .then(
+      function(datos){
+        console.log(' fetch, then 2: ');
+        console.log( datos );
+        /////////////////////////try catch////////////////////////
+        let datosJSOBJ;
+        try{
+          datosJSOBJ = JSON.parse( datos );
+        }
+        catch( err ){
+          throw new Error( err + '<br><br>' + datos ); 
+        }
+        //////////////////////////////////////////////////////////
+        if(datosJSOBJ.nepeCoreUpdated && datosJSOBJ.queDondeUpdated && (datosJSOBJ.videoInserted || datosJSOBJ.videoUpdated  || datosJSOBJ.videoRemoved || datosJSOBJ.videoNothingDone )){
+          window.location.href = window.location.pathname + '?look=viewNepe&nepeId=' + datosJSOBJ.nepeId;
+        }else{
+          //falta algo ?
+          //feedback('form#updateNepeForm h2', datosJSOBJ.feedback);
+        } 
+      })
+      .catch(
+      function(error){
+        const href = encodeAndGetErrorPath(error);
+        window.location.href = href;
+      });
+    }//postea function
 	
 
 
@@ -292,22 +257,28 @@ fotoBoton.addEventListener('click', function(evento){
 	fotoInput.click();
 });
 
-/*
-jQuery('fieldset#submitButtonFieldset button').on('click', function(evento){
-	jQuery.showThemSections();
+fotoInput.addEventListener('input', function(evento){
+      console.log("fotoInput listener...");
+      //fotos
+      //let filesOnFotoArr = formData.getAll("fotoArr[]");
+      //la linea de abajo para filesOnFotoArr, works tambien
+      //let select = 'fieldset#fotoFieldset   input[name^=fotoArr]';
+      let filesOnFotoArr = fotoInput.files;
+      if( filesOnFotoArr.length > 0 ){
+        ponScript('js/nepe/resizeImage.js');
+        console.log("procezando: " + filesOnFotoArr.length + " files...");
+      }else{ //if( filesOnFotoArr.length === 0){
+        formData.delete("fotoArr[]");     //tested with isset on php
+        formData.delete("MAX_FILE_SIZE");
+      }
+      //end of fotos
 });
-*/
-/*
-let formaStr = 'form#nepeForm';
-//validation logic functions are run as handlers to change events
-let $redInputs = jQuery( formaStr + ' input[name^=red]');
-$redInputs.on('change', function(evento){
-	jQuery.haveAtLeast1Red(formaStr);
+
+
+
+
+
+let botonSubmit = document.querySelector('section#submitButtonFieldset   button[type=submit]');
+botonSubmit.addEventListener('click', function(evento){
+	showThemSections();
 });
-*/
-/*
-let $fotoInput = jQuery( formaStr + ' input#fotoArrId');
-$fotoInput.on('change', function(evento){
-	jQuery.have5OrLessImages(formaStr);
-});
-*/
