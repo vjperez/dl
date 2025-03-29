b17c771ace10c482eb281233b9941d82f79a7e8e
victor j. perez <vijapefi@gmail.com>
2025-03-29 17:55:44 -0400

    restart sequences

---
+++ utils/queries/restartSeq.sql
@@ -0,0 +1,12 @@
+alter sequence dueno_id_seq  RESTART;
+alter sequence dueno_nepe_id_seq  RESTART;
+alter sequence social_id_seq  RESTART;
+alter sequence nepe_id_seq  RESTART;
+
+alter sequence video_id_seq  RESTART;
+alter sequence foto_id_seq  RESTART;
+
+alter sequence nepe_que_id_seq  RESTART;
+alter sequence nepe_donde_id_seq  RESTART;
+alter sequence que_id_seq  RESTART;
+alter sequence donde_id_seq  RESTART;
