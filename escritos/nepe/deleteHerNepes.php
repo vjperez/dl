bae531a095b34e89f0061e1c9c9365da7d479734
victor j. perez <vijapefi@gmail.com>
2025-03-31 13:14:47 -0400

    delete her nepes images

--- escritos/nepe/deleteHerNepes.php
+++ escritos/nepe/deleteHerNepes.php
@@ -21,29 +21,24 @@ if(isset($_SESSION['dueno_id'])){
 		$cuantos = 0;
 		while( $fila = pg_fetch_row($recurso_ghn) ){  
       $nepe_to_delete = $fila[0];
-
       //////////////////// deleting que and donde ////////////////
       $toBeIdsArr = array();// empty for both que and donde
-
       $areIdsArr  = getCurrentIds('Que',   $nepe_to_delete, $cnx);
       makeAndRemoveLinks('Que', $nepe_to_delete, $areIdsArr, $toBeIdsArr, $cnx);
-      
       $areIdsArr  = getCurrentIds('Donde',   $nepe_to_delete, $cnx);
       makeAndRemoveLinks('Donde', $nepe_to_delete, $areIdsArr, $toBeIdsArr, $cnx);
       //////////////////// deleting que and donde ////////////////
-
       $recurso_dn = pg_execute($cnx, "preparadoQueryDeleteNepe", array($nepe_to_delete));
       if($recurso_dn){		 
         if(pg_affected_rows($recurso_dn) == 1){
           $cuantos++;
-              /*
-                // delete foto files is done by "preparadoQueryDeleteNepe", when nepe is deleted
+                // delete foto files is done by "preparadoQueryDeleteNepe",
+                // when nepe is deleted
                 require_once '../configConstants/constants.php';
                 $fotoTarget = $fotos_subidas_dir . $nepe_to_delete . '[abcdefgh].';
                 foreach(glob($fotoTarget . '*') as $fotoToErase){
                   if(file_exists ($fotoToErase)) unlink($fotoToErase);
                 }
-              */
         }		
       }else{
         pg_close($cnx); //maybe not needed but doesn't hurt	
