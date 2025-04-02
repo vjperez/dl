a0872170b0482691f1922c3cfbaadbc26e11bd3a
victor j. perez <vijapefi@gmail.com>
2025-04-02 14:34:35 -0400

    add nepe row on errors

--- js/error.js
+++ js/error.js
@@ -1,3 +1,45 @@
+const nepeId = -2147483648;
+
+let urlParams = new URLSearchParams('escritos/nepe/read/getNepe.php');
+urlParams.set("nepeId", nepeId);
+fetch('escritos/nepe/read/getNepe.php' + '?' + urlParams.toString() )
+.then(
+function(respuesta){
+  console.log('view nepe fetch, then 1');
+  console.log(respuesta);
+  return respuesta.json();
+})
+.then(
+function(dato){
+  console.log('view nepe fetch, then 2: ');
+  console.log(dato);
+  addNepeRow(dato); 
+})
+.catch(
+function(error){
+  //error on error loop!
+  //const href = encodeAndGetErrorPath(error);
+  //window.location.href = href;
+});
+
+function addNepeRow(elNepe){
+  let tableRow = '';
+  //console.log(index + '::' + elNepe.nepeFotoName);
+  let link = '"portada.html?look=viewNepe&nepeId=' + elNepe.nepeId  + '"';
+  tableRow += '<tr class="texto"><td>' 
+  + '<a href=' + link + '>' 
+  + elNepe.nombre + '</a>   <span class="diasOld">'  +   elNepe.revisado  +  '</span>'
+  + '</td></tr>'; 
+
+  tableRow +=  '<tr class="foto"><td>' 
+  + '<a href=' + link + '>' 
+  + '<img src="imagenes/nepe/subidas/' + elNepe.losFoto[0] + '">'
+  + '</a>' 
+  + '</td></tr>'; 
+  tableRow += '<tr><td></td></tr><tr><td></td></tr>';
+	document.querySelector('table.subArea').innerHTML = tableRow;
+}
+
 function appendDebugErrors( msg ){
     // <br />\n  changed to  <br>  
     msg = msg.replace(new RegExp('<br \/>\r?\n','g'), '<br>');
@@ -8,9 +50,9 @@ function appendDebugErrors( msg ){
     msg = msg.replace(/Stack trace:/, '<br>Stack trace:'); 
 
     
-    losLis = '<br><hr>';
+    losLis = '<br><hr><br>';
     losLis += '<li>Error:<br>' + msg + '</li>';
-    losLis += '<hr>';
+    losLis += '<br><hr>';
     document.querySelector('#containerForErrors').insertAdjacentHTML('beforeend', losLis);
 }
 
