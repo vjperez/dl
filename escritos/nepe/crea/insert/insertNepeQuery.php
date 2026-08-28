2d33289de247947520646b7b8b91e24b4e7e7847
victor j perez <vijapefi@gmail.com>
2026-08-27 22:09:26 -0400

    remove generated value on insert

--- escritos/nepe/crea/insert/insertNepeQuery.php
+++ escritos/nepe/crea/insert/insertNepeQuery.php
@@ -3,8 +3,8 @@
 
 // inserts a into db a nepe
 $queryInsertNepe = "INSERT INTO
-	nepe (nombre, cuando, su_casa, desde_casa, creado, revisado, activo, vence)
- 	VALUES ($1, $2, $3, $4,  NOW()::date,  NOW()::date,  TRUE,  365 + NOW()::date )";
+	nepe (nombre, cuando, su_casa, desde_casa, creado, revisado, activo )
+ 	VALUES ($1, $2, $3, $4,  NOW()::date,  NOW()::date,  TRUE )";
 pg_prepare($cnx, "preparadoQueryInsertNepe", $queryInsertNepe);
 
 
