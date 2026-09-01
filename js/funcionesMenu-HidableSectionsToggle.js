f97da5ae92630246b975be6822d07b4f1a46df16
victor j perez <vijapefi@gmail.com>
2026-09-01 14:58:33 -0400

    show & hide sections on single loop

--- js/funcionesMenu-HidableSectionsToggle.js
+++ js/funcionesMenu-HidableSectionsToggle.js
@@ -28,12 +28,28 @@ function showMenu() {
 
 
 function hideThemSections() {
+  let nhels = document.querySelectorAll('.notHidable');
+  nhels.forEach(
+    function(nhel){
+      let hidablecirclechild = nhel.querySelector(':scope > .fa-chevron-circle-up');
+      hidablecirclechild.style.display = 'none';  
+          hidablecirclechild = nhel.querySelector(':scope > .fa-chevron-circle-down');
+      hidablecirclechild.style.display = '';   // probably was already sown ... but doesn't hurt
+    
+      let hidablesibling = nhel.nextElementSibling; 
+      while (hidablesibling) { //  one of the sibling of notHidable ... is hidable
+          if (hidablesibling.classList.contains('hidable')) {
+              hidablesibling.style.display = 'none';
+              break; // exit the loop - only gets first match
+          }
+          hidablesibling = hidablesibling.nextElementSibling; // next sibling
+      }
+    }
+  );
+  /*
   let els = document.querySelectorAll('.hidable');
   els.forEach(
     function(el){
-      //when selecting .notHidable... was doing these
-      //const hidableSibling = el.nextElementSibling;
-      //if(hidableSibling.classList.contains('hidable')) hidableSibling.style.display = 'none';
       el.style.display = 'none';  
     }
   );
@@ -49,16 +65,33 @@ function hideThemSections() {
       el.style.display = ''; 
     }
   );
+  */
 }
 
 
 function showThemSections() {
+  let nhels = document.querySelectorAll('.notHidable');
+  nhels.forEach(
+    function(nhel){
+      let hidablecirclechild = nhel.querySelector(':scope > .fa-chevron-circle-down');
+      hidablecirclechild.style.display = 'none';  
+          hidablecirclechild = nhel.querySelector(':scope > .fa-chevron-circle-up');
+      hidablecirclechild.style.display = '';// needed if coming from hidden state    
+    
+      let hidablesibling = nhel.nextElementSibling;
+      while (hidablesibling) { //  one of the sibling of notHidable ... is hidable
+          if (hidablesibling.classList.contains('hidable')) {
+              hidablesibling.style.display = '';
+              break; // exit the loop - only gets first match
+          }
+          hidablesibling = hidablesibling.nextElementSibling; // next sibling
+      }
+    }
+  );
+  /*
   let els = document.querySelectorAll('.hidable');
   els.forEach(
     function(el){
-      //when selecting .notHidable... was doing these
-      //const hidableSibling = el.nextElementSibling;
-      //if(hidableSibling.classList.contains('hidable')) hidableSibling.style.display = 'block';
       el.style.display = '';
     }
   );
@@ -74,6 +107,7 @@ function showThemSections() {
       el.style.display = 'none'; 
     }
   );
+  */
 }
 
 
