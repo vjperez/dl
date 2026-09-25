948a451fe948313586ed42467395630755c8b17f
victor j perez <vijapefi@gmail.com>
2026-09-25 06:44:04 -0400

    dueno insert query

--- escritos/dueno/crea/duenoInsertQuery.php
+++ escritos/dueno/crea/duenoInsertQuery.php
@@ -1,9 +1,9 @@
 <?php	
 // query to insert new dueno on db
-$queryInsert = "INSERT INTO
-	dueno(nombre, clave, first_log, last_log)
+$queryDuenoInsert = "INSERT INTO
+	dueno(emilio, clave, first_log, last_log)
 	VALUES($1, $2, NOW()::date, NOW()::date)";
 	// google  'RETURNING' clause on the INSERT INTO statement, and avoid a currval query
 
-pg_prepare($cnx, "preparadoQueryInsert", $queryInsert);
+pg_prepare($cnx, "preparadoQueryDuenoInsert", $queryDuenoInsert);
 ?>
