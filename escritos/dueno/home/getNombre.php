e89fd72ab918f85878ed11d23353e58ae495fb80
victor j perez <vijapefi@gmail.com>
2026-09-25 14:58:15 -0400

    get nombre at home

---
+++ escritos/dueno/home/getNombre.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+if(isset($_SESSION['dueno_id'])){ 
+	if(isset( $_REQUEST['userNumber'] )){
+		$dueno_to_query = $_REQUEST['userNumber'];  // cuando el admin necesita el nombre de otro userNumber
+	}else{
+		$dueno_to_query = $_SESSION['dueno_id'];  // cuando se usa nombre desde home, ahi, no se postea un userNumber
+	}
+
+	require_once '../../conecta/conecta.php';
+	
+	require_once '../read/getNombreQuery.php';
+	$recurso = pg_execute($cnx, "preparadoQueryGetNombre", array($dueno_to_query));
+	if($recurso){
+		$fila = pg_fetch_row($recurso);
+
+		if( $fila !== false){
+			$elUsername = $fila[0];
+		}else{
+			$elUsername = false;
+		}
+
+		pg_close($cnx);
+		echo json_encode($elUsername);
+	}else{
+		pg_close($cnx);
+		throw new Exception('Mal query.  Sin RECURSO para preparadoQueryGetNombre en: ' . __FILE__  );
+	}
+}else{
+	throw new Exception('Session no seteada en: ' . __FILE__  );
+}
+?>
